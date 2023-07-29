db3701dc8e27ca53a3974862196b122015b028e3
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-07-30 01:43:00 +0900

    不要なコメントの削除　elseの見直し　ステータスコード追記

--- backend/app/Http/Controllers/ItemController.php
+++ backend/app/Http/Controllers/ItemController.php
@@ -22,26 +22,21 @@ class ItemController extends Controller
     {
         // requestにcategorynameが入っていればカテゴリーで検索
         if ($request->has('categoryname')) {
-            // カテゴリーを検索
+
             $category = Category::where('category_name', $request->get('categoryname'))->first();
 
-            // カテゴリーが見つかれば、カテゴリーに関連するアイテムを取得
-            if ($category) {
-                // カテゴリにー関連するアイテムを取得し、関連データも一緒にロード
-                $items = Item::getItemDataWithRelations($category->items());
-                return response()->json($items, 200);
-            } else {
-                return response()->json(['message' => 'カテゴリーが見つかりませんでした']);
+            if (!$category) {
+                return response()->json(['message' => 'カテゴリーが見つかりませんでした'], 404);
             }
+
+            $items = Item::getItemDataWithRelations($category->items());
+            return response()->json($items, 200);
         }
 
         //カテゴリーが入ってなければ全件渡す
-        else {
-            $allItems = Item::query();
-            // 関連データも一緒にロード
-            $items = Item::getItemDataWithRelations($allItems);
-            return response()->json($items, 200);
-        }
+        $allItems = Item::query();
+        $items = Item::getItemDataWithRelations($allItems);
+        return response()->json($items, 200);
     }
 
     /**
@@ -55,7 +50,7 @@ class ItemController extends Controller
         // 既に登録されていればエラー ASINで検索
         $item = Item::where('asin', $request['itemDatas']['asin'])->first();
         if ($item) {
-            return response()->json(['message' => '既に登録されています'], 400);
+            return response()->json(['message' => '既に登録されています'], 409);
         }
 
         // requestからbrandName, itemCategoryName, subCategoryNameを取得して、brand, category, subCategoryを検索
@@ -63,20 +58,17 @@ class ItemController extends Controller
         $category = Category::where('category_name', $request['itemDatas']['itemCategoryName'])->first();
         $subCategory = SubCategory::where('sub_category_name', $request['itemDatas']['subCategoryName'])->first();
 
-        // brand, category, subCategoryが存在するか確認
         if (!$brand || !$category || !$subCategory) {
             return response()->json(['message' => 'Brand, Category, or SubCategory not found'], 404);
         }
 
-        // requestからitemDatasを取得して、itemを作成
         $data = $request['itemDatas'];
         $data['brand_id'] = $brand->brand_id;
         $data['category_id'] = $category->category_id;
         $data['sub_category_id'] = $subCategory->sub_category_id;
-
         Item::createItemData($data);
 
-        return response()->json(['message' => '商品登録が完了しました']);
+        return response()->json(['message' => '商品登録が完了しました'], 201);
     }
 
     /**
@@ -87,49 +79,13 @@ class ItemController extends Controller
      */
     public function show($id)
     {
-        // Itemテーブルからitem_idを取得
         $item = Item::find($id);
 
-        // itemがみつかれば、商品詳細を取得
-        if ($item) {
-            $itemData = Item::getItemDataWithRelations($item);
-            return response()->json($itemData, 200);
-        } else {
-            return response()->json(['message' => '商品が見つかりませんでした']);
+        if (!$item) {
+            return response()->json(['message' => '商品が見つかりませんでした'], 404);
         }
-    }
 
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $itemData = Item::getItemDataWithRelations($item);
+        return response()->json($itemData, 200);
     }
 }

d4a0e355e09419b6a964f8584ea1076f2d0ca95a
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-07 01:20:24 +0900

    不要なuseと引数名の修正

--- backend/app/Http/Controllers/ItemViewCountController.php
+++ backend/app/Http/Controllers/ItemViewCountController.php
@@ -2,16 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Item;
-use Illuminate\Support\Facades\Log;
-
-
 class ItemViewCountController extends Controller
 {
     // 商品ページのview_countを更新する
 
-    public function update($id)
+    public function update($item_id)
     {
         // Item::viewCountIncrement($id);
         return response()->json(['message' => '閲覧回数を更新しました。'], 200);

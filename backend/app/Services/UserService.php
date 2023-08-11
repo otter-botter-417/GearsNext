871cacc3fe16c9cc2edda241f73431f019c56586
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-11 22:20:06 +0900

    FavoriteServiceクラスの実装につきUserモデルからメソッドの移動

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -16,10 +16,15 @@ class UserService
      */
     protected $userRepository;
 
+    private $favoriteItemService;
+
     public function __construct(
-        UserRepositoryInterface $userRepository
+        UserRepositoryInterface $userRepository,
+        FavoriteItemService $favoriteItemService
+
     ) {
         $this->userRepository = $userRepository;
+        $this->favoriteItemService = $favoriteItemService;
     }
 
     /**
@@ -38,5 +43,41 @@ class UserService
         $this->userRepository->ensureEmailNotExists($email);
 
         $this->userRepository->createUserData($userFirebaseId, $name, $email);
+    }
+
+    /**
+     * お気に入りに商品を追加
+     * @param  string $userFirebaseId
+     * @param  int    $itemId
+     * @throws UserNotFoundException ユーザーが見つからない場合にスローされます。
+     * @throws ItemNotFoundException 商品が見つからない場合にスローされます。
+     * @throws ItemAlreadyFavoritedException お気に入りに商品が存在する場合にスローされます。
+     */
+    public function addFavoriteItem(string $userFirebaseId, int $itemId): void
+    {
+        $this->favoriteItemService->addFavoriteItem($userFirebaseId, $itemId);
+    }
+
+    /**
+     * お気に入りから商品を削除
+     * @param  string $userFirebaseId
+     * @param  int    $itemId
+     * @throws UserNotFoundException ユーザーが見つからない場合にスローされます。
+     * @throws ItemNotFoundException 商品が見つからない場合にスローされます。
+     * @throws ItemNotFavoritedException お気に入りに商品が存在しない場合にスローされます。
+     */
+    public function removeFavoriteItem(string $userFirebaseId, int $itemId): void
+    {
+        $this->favoriteItemService->removeFavoriteItem($userFirebaseId, $itemId);
+    }
+
+    /**
+     * お気に入り一覧を取得
+     * @param  string $userFirebaseId
+     * @return array
+     */
+    public function getFavoriteItems(string $userFirebaseId): array
+    {
+        return $this->favoriteItemService->getFavoriteItems($userFirebaseId)->toArray();
     }
 }

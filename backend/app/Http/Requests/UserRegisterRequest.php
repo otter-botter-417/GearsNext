ba91f556216cc191d45e4ef4df31522d42253cba
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-16 20:11:09 +0900

    firebaseを使わずlaravelでユーザー登録するように変更した為、ルールの修正

--- backend/app/Http/Requests/UserRegisterRequest.php
+++ backend/app/Http/Requests/UserRegisterRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserRegisterRequest extends FormRequest
 {
@@ -20,9 +22,9 @@ class UserRegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            'userFirebaseId' => 'required|string|max:50',
-            'name' => 'required|string|max:20',
-            'email' => 'required|email|max:255',
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|max:255|unique:users,email,',
+            'password' => 'required|string|between:6,100',
         ];
     }
 
@@ -32,15 +34,21 @@ class UserRegisterRequest extends FormRequest
     public function messages()
     {
         return [
-            'userFirebaseId.required' => 'firebaseIDは必須です。',
-            'userFirebaseId.string' => 'firebaseIDは文字列である必要があります。',
-            'userFirebaseId.max' => 'firebaseIDは50文字以内である必要があります。',
             'name.required' => '名前は必須です。',
             'name.string' => '名前は文字列である必要があります。',
             'name.max' => '名前は20文字以内である必要があります。',
             'email.required' => 'メールアドレスは必須です。',
             'email.email' => 'メールアドレスの形式が正しくありません。',
             'email.max' => 'メールアドレスは255文字以内である必要があります。',
+            'email.unique' => 'このメールアドレスは既に登録されています。',
+            'password.required' => 'パスワードは必須です。',
+            'password.string' => 'パスワードは文字列である必要があります。',
+            'password.between' => 'パスワードは6文字以上100文字以内である必要があります。',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 }

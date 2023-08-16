13c9abf0fdb1dcce75091559fc040fbb2b7113bc
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-16 23:41:28 +0900

    不要な行を削除

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,23 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ItemViewCountController;
 use App\Http\Controllers\UserInventoryController;
 use App\Http\Controllers\FavoriteItemController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 // ユーザー関連のルート
 Route::prefix('user')->group(function () {

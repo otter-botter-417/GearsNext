42468476f56b5616101c160344e3d3a6f60e6778
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-18 17:45:42 +0900

    LayoutTableの実装

---
+++ backend/database/migrations/2023_08_17_004548_create_layouts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('layouts', function (Blueprint $table) {
+            $table->increments('layout_id');
+            $table->text('text', 50);
+            // $table->unsignedInteger('image_map_id');
+            // $table->foreign('image_map_id')
+            //     ->references('image_map_id')
+            //     ->on('image_maps')
+            //     ->onDelete('cascade');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                ->references('user_id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->integer('favorite_count')->default(0);
+            $table->integer('view_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('layouts');
+    }
+};

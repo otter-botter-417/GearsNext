b9af7d1a077f819385446b146ff3129e356f9e64
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-18 17:57:50 +0900

    item_layout_tableの実装

---
+++ backend/database/migrations/2023_08_17_131232_create_item_layout_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_layout', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('item_id');
+            $table->unsignedInteger('layout_id');
+            $table->foreign('item_id')->references('item_id')->on('items')->onDelete('cascade');
+            $table->foreign('layout_id')->references('layout_id')->on('layouts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_layout');
+    }
+};

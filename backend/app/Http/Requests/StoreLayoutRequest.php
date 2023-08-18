454c386be1839561b804372e923894257faab615
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-18 21:14:41 +0900

    updateとstoreで受け取るリクエストの構造を変更

--- backend/app/Http/Requests/StoreLayoutRequest.php
+++ backend/app/Http/Requests/StoreLayoutRequest.php
@@ -24,9 +24,11 @@ class StoreLayoutRequest extends FormRequest
     public function rules()
     {
         return [
-            'itemIds' => 'required|array',
-            'itemIds.*' => 'required|integer',
             'text' => 'string|max:255',
+            'items' => 'required|array',
+            'items.*.item_id' => 'required|integer',
+            'items.*.x_position' => 'required|integer',
+            'items.*.y_position' => 'required|integer',
         ];
     }
 
@@ -36,12 +38,17 @@ class StoreLayoutRequest extends FormRequest
     public function messages()
     {
         return [
-            'itemId.required' => '商品IDは必須です',
-            'itemId.array' => '商品IDは配列である必要があります。',
-            'itemId.*.required' => '商品IDは必須です',
-            'itemId.*.integer' => '商品IDは整数である必要があります。',
             'text.string' => 'テキストは文字列である必要があります。',
             'text.max' => 'テキストは255文字以内である必要があります。',
+            'items.required' => '商品情報は必須です',
+            'items.array' => '商品情報は配列である必要があります。',
+            'items.*.item_id.required' => '商品IDは必須です',
+            'items.*.item_id.integer' => '商品IDは整数である必要があります。',
+            'items.*.x_position.required' => 'X座標は必須です',
+            'items.*.x_position.integer' => 'X座標は整数である必要があります。',
+            'items.*.y_position.required' => 'Y座標は必須です',
+            'items.*.y_position.integer' => 'Y座標は整数である必要があります。',
+
         ];
     }
 }

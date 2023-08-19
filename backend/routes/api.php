e69b6823a445284a50936427823caf85ddd3bc49
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-20 02:58:10 +0900

    パブリックなレイアウト関連のルートを追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\UserInventoryController;
 use App\Http\Controllers\FavoriteItemController;
 use App\Http\Controllers\LayoutController;
+use App\Http\Controllers\PublicLayoutController;
 
 // ユーザー関連のルート
 Route::prefix('user')->group(function () {
+    // 認証が不要なユーザー関連のルート
     Route::post('register', [UserController::class, 'register']);
     Route::post('login', [UserController::class, 'login']);
+
+    // 認証が必要なユーザー関連のルート
     Route::middleware(['auth:api'])->group(function () {
         Route::post('logout', [UserController::class, 'logout']);
         Route::post('update', [UserController::class, 'update']);
@@ -18,16 +22,25 @@ Route::prefix('user')->group(function () {
     });
 });
 
+// パブリックなレイアウト関連のルート
+Route::get('layout', [PublicLayoutController::class, 'index']);
+Route::get('layout/{id}', [PublicLayoutController::class, 'show'])
+    ->middleware('AttachUserIdToRequest');
+
+
 // アイテム関連のルート
 Route::apiResource('items', ItemController::class)->only(['index', 'store']);
 Route::get('items/{id}', [ItemController::class, 'show'])->middleware('AttachUserIdToRequest');
 
-
+// 認証が必要なルート
 Route::middleware(['auth:api'])->group(function () {
+    // ユーザーのレイアウト関連のルート
     Route::apiResource('user/layout', LayoutController::class);
+
     // ユーザーのインベントリ関連のルート
     Route::apiResource('user/inventory', UserInventoryController::class)
         ->only(['index', 'store', 'destroy']);
+
     // ユーザーのお気に入りアイテム関連のルート
     Route::apiResource('user/favorite/items', FavoriteItemController::class)
         ->only(['index', 'store', 'destroy']);

105ae03b184753ce75fdc1e94f1d0f0682951330
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-22 21:59:53 +0900

    各メソッド名を統一感のある命名に変更

--- backend/tests/Feature/UserInventoryControllerTest.php
+++ backend/tests/Feature/UserInventoryControllerTest.php
@@ -3,9 +3,16 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Traits\AuthorizesRequests;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
+/**
+ * UserInventoryControllerTest
+ *
+ * このクラスは、ユーザーのインベントリ管理に関連するエンドポイントのテストを担当します。
+ * それには、インベントリへのアイテムの追加、削除、取得などの操作が含まれます。
+ * AuthorizesRequestsトレイトを使用して、認証済みのリクエストをシミュレートします。
+ */
 class UserInventoryControllerTest extends TestCase
 {
     use RefreshDatabase, AuthorizesRequests;
@@ -21,27 +28,27 @@ class UserInventoryControllerTest extends TestCase
      * 持っている商品を追加
      * @covers \App\Http\Controllers\UserInventoryController::store
      */
-    public function test_user_can_add_item_to_inventory()
+    public function test_can_add_item_to_user_inventory()
     {
-        $this->assertDatabaseHas('user_inventories', ['item_id' => 1]);
+        $this->assertDatabaseHas('user_inventories', ['item_id' => 1, 'user_id' => $this->userId]);
     }
 
     /**
      * 持っている商品から削除
      * @covers \App\Http\Controllers\UserInventoryController::destroy
      */
-    public function test_destroy_delete_an_user_inventory()
+    public function test_can_delete_item_from_user_inventory()
     {
         $response = $this->authorizedRequest('DELETE', '/api/user/inventory/1');
         $response->assertStatus(204);
-        $this->assertDatabaseMissing('user_inventories', ['item_id' => 1]);
+        $this->assertDatabaseMissing('user_inventories', ['item_id' => 1, 'user_id' => $this->userId]);
     }
 
     /**
      * ユーザーの持っている商品のデータを取得
      * @covers \App\Http\Controllers\UserInventoryController::index
      */
-    public function test_index_get_user_inventories()
+    public function test_can_get_user_inventories()
     {
         $response = $this->authorizedRequest('GET', '/api/user/inventory');
         $response->assertStatus(200)
@@ -61,10 +68,9 @@ class UserInventoryControllerTest extends TestCase
      * 持っている商品に追加時に商品が見つからない場合
      * @covers \App\Http\Controllers\UserInventoryController::store
      */
-    public function test_store_add_an_user_inventory_with_not_found_item()
+    public function test_cannot_add_non_existent_item_to_user_inventory()
     {
         $response = $this->authorizedRequest('POST', '/api/user/inventory/999');
-
         $response->assertStatus(404);
     }
 }

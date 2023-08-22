71f3e13007c4913de5ae6c05acab8d85f52d06a7
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-22 22:37:05 +0900

    各メソッドのDocでuserIdの型がstringになっていたのを、intに変更

--- backend/app/Contracts/FavoriteItemRepositoryInterface.php
+++ backend/app/Contracts/FavoriteItemRepositoryInterface.php
@@ -6,17 +6,15 @@ interface FavoriteItemRepositoryInterface
 {
     /**
      * お気に入りに商品を追加
-     * @param  string $userId
-     * @param  int    $itemId
-     * @throws ItemNotFoundException 商品が見つからない場合
-     * @throws ItemAlreadyFavoritedException お気に入りに商品が存在する場合
+     * @param  int  $userId
+     * @param  int  $itemId
      */
     public function addFavoriteItemData(int $userId, int $itemId): void;
 
     /**
      * お気に入りから商品を削除
-     * @param  string $userId
-     * @param  int    $itemId
+     * @param  int  $userId
+     * @param  int  $itemId
      * @return void
      * @throws ItemNotFavoritedException お気に入りに商品が存在しない場合
      */

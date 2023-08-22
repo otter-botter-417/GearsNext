01b2d927ce3178beeca36c7527dfd06c0cfb59fa
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-22 21:56:36 +0900

    Docで各メソッドに説明を追記

--- backend/tests/Traits/AuthorizesRequests.php
+++ backend/tests/Traits/AuthorizesRequests.php
@@ -6,19 +6,40 @@ use App\Models\User;
 use App\Models\Item;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
+/**
+ * AuthorizesRequests
+ * 
+ * このトレイトは、テストケースで認証済みのリクエストをシミュレートするための機能を提供します。
+ * JWT認証を使用して、指定されたユーザーとしてリクエストを行うことができます。
+ * このトレイトは、APIエンドポイントの認証が必要なテストに使用されます。
+ * @mixin \Illuminate\Foundation\Testing\TestCase
+ * @property int $userId
+ */
 trait AuthorizesRequests
 {
-    protected  $token;
-    protected  $user;
+    private  $token;
+    private  $user;
+    protected  $userId;
 
+    /**
+     * テスト前にユーザー登録を行いトークンを取得
+     * @return void
+     */
     protected function initializeAuthorization(): void
     {
         $this->seed();
         $this->user = User::factory()->create();
         $this->token = JWTAuth::fromUser($this->user);
-        Item::factory(3)->create();
+        $this->userId  = $this->user->user_id;
     }
 
+    /**
+     * ユーザーエンドポイントにリクエストを送信する
+     * @param string $method HTTPメソッド（GET, POST, PUT, DELETEなど）
+     * @param string $url
+     * @param array $data
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
     private function authorizedRequest($method, $url, $data = [])
     {
         return $this->withHeaders([

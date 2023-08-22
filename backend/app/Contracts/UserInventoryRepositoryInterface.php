465292f9cc8176240ed755f804cb32702964c4e4
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-22 19:47:00 +0900

    不要なDocを削除

--- backend/app/Contracts/UserInventoryRepositoryInterface.php
+++ backend/app/Contracts/UserInventoryRepositoryInterface.php
@@ -8,7 +8,6 @@ interface UserInventoryRepositoryInterface
      * 持っている商品に追加
      * @param  int  $userId
      * @param  int  $itemId
-     * @throws ItemAlreadyFavoritedException お気に入りに商品が存在する場合
      */
     public function addUserInventoryData(int $userId, int $itemId): void;
 
@@ -17,7 +16,7 @@ interface UserInventoryRepositoryInterface
      * @param  int  $userId
      * @param  int  $itemId
      * @return void
-     * @throws ItemNotFavoritedException お気に入りに商品が存在しない場合
+     * @throws ItemNotFavoritedException 持っている商品に存在しない
      */
     public function removeUserInventoryData(int $userId, int $itemId): void;
 

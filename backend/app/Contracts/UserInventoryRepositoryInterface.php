ff74221659a980ade69b5517a100098ea1150f2e
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-22 19:46:06 +0900

    持っている物テーブルの確認メソッドが不要になった為、削除

--- backend/app/Contracts/UserInventoryRepositoryInterface.php
+++ backend/app/Contracts/UserInventoryRepositoryInterface.php
@@ -5,26 +5,17 @@ namespace App\Contracts;
 interface UserInventoryRepositoryInterface
 {
     /**
-     * 既に持っている商品に追加されているか確認
-     * @param int $userId
-     * @param int $itemId
-     * @return void
-     */
-    public function userInventoryAlreadyExists(int $userId, int $itemId): void;
-
-    /**
      * 持っている商品に追加
-     * @param  string $userId
-     * @param  int    $itemId
-     * @throws ItemNotFoundException 商品が見つからない場合
+     * @param  int  $userId
+     * @param  int  $itemId
      * @throws ItemAlreadyFavoritedException お気に入りに商品が存在する場合
      */
     public function addUserInventoryData(int $userId, int $itemId): void;
 
     /**
      * 持っている商品から削除
-     * @param  string $userId
-     * @param  int    $itemId
+     * @param  int  $userId
+     * @param  int  $itemId
      * @return void
      * @throws ItemNotFavoritedException お気に入りに商品が存在しない場合
      */

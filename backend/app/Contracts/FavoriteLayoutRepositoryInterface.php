b0d9db5b9a41d3b8bc40321ccec12e0af1091226
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-22 23:13:09 +0900

    favoriteLayoutAlreadyExistsメソッドは不要になったため、削除

--- backend/app/Contracts/FavoriteLayoutRepositoryInterface.php
+++ backend/app/Contracts/FavoriteLayoutRepositoryInterface.php
@@ -7,14 +7,6 @@ use App\Models\Layout;
 interface FavoriteLayoutRepositoryInterface
 {
     /**
-     * 既にお気に入りにレイアウトが追加されているか確認
-     * @param int $userId
-     * @param int $layoutId
-     * @return void
-     */
-    public function favoriteLayoutAlreadyExists(int $userId, int $layoutId): void;
-
-    /**
      * お気に入りにレイアウトを追加
      * @param  int $userId
      * @param  int $layoutId

d20f7f3861e8dc352c6af33026fccc14671a005a
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-23 21:37:32 +0900

    メッセージの生成部分を共通化

--- backend/app/Http/Requests/ItemRegisterRequest.php
+++ backend/app/Http/Requests/ItemRegisterRequest.php
@@ -52,53 +52,64 @@ class ItemRegisterRequest extends FormRequest
     public function messages()
     {
         return [
-            'baseData.item_name.required' => '商品名は必須です。',
-            'baseData.item_name.string' => '商品名は文字列である必要があります。',
+            'baseData.item_name.required' => $this->requiredMessage('商品名'),
+            'baseData.item_name.string' => $this->stringMessage('商品名'),,
             'baseData.item_name.max' => '商品名は最大255文字までです。',
-            'baseData.asin.required' => 'ASINは必須です。',
+            'baseData.asin.required' => $this->requiredMessage('ASIN'),
             'baseData.asin.size' => 'ASINは10文字である必要があります。',
-            'baseData.asin.string' => 'ASINは文字列である必要があります。',
-            'baseData.image_name.required' => '画像名は必須です。',
+            'baseData.asin.string' => $this->stringMessage('ASIN'),
+            'baseData.image_name.required' => $this->requiredMessage('画像名'),
             'baseData.image_name.max' => '画像名は最大255文字までです。',
-            'baseData.image_name.string' => '画像名は文字列である必要があります。',
-            'baseData.price.required' => '価格は必須です。',
-            'baseData.price.numeric' => '価格は数値である必要があります。',
-            'baseData.open_width.required' => '展開時の幅は必須です。',
-            'baseData.open_width.numeric' => '展開時の幅は数値である必要があります。',
-            'baseData.open_depth.required' => '展開時の奥行きは必須です。',
-            'baseData.open_depth.numeric' => '展開時の奥行きは数値である必要があります。',
-            'baseData.open_height.required' => '展開時の高さは必須です。',
-            'baseData.open_height.numeric' => '展開時の高さは数値である必要があります。',
-            'baseData.storage_width.required' => '収納時の幅は必須です。',
-            'baseData.storage_width.numeric' => '収納時の幅は数値である必要があります。',
-            'baseData.storage_depth.required' => '収納時の奥行きは必須です。',
-            'baseData.storage_depth.numeric' => '収納時の奥行きは数値である必要があります。',
-            'baseData.storage_height.required' => '収納時の高さは必須です。',
-            'baseData.storage_height.numeric' => '収納時の高さは数値である必要があります。',
-            'baseData.weight.required' => '重量は必須です。',
-            'baseData.weight.numeric' => '重量は数値である必要があります。',
-            'baseData.brand_name.required' => 'ブランド名は必須です。',
-            'baseData.brand_name.string' => 'ブランド名は文字列である必要があります。',
+            'baseData.image_name.string' => $this->stringMessage('画像名'),
+            'baseData.price.required' => $this->requiredMessage('価格'),
+            'baseData.price.numeric' => $this->numericMessage('価格'),
+            'baseData.open_width.required' => $this->requiredMessage('展開時の幅'),
+            'baseData.open_width.numeric' => $this->numericMessage('展開時の幅'),
+            'baseData.open_depth.required' => $this->requiredMessage('展開時の奥行き'),
+            'baseData.open_depth.numeric' => $this->numericMessage('展開時の奥行き'),
+            'baseData.open_height.required' => $this->requiredMessage('展開時の高さ'),
+            'baseData.open_height.numeric' => $this->numericMessage('展開時の高さ'),
+            'baseData.storage_width.required' => $this->requiredMessage('収納時の幅'),
+            'baseData.storage_width.numeric' => $this->numericMessage('収納時の幅'),
+            'baseData.storage_depth.required' => $this->requiredMessage('収納時の奥行き'),
+            'baseData.storage_depth.numeric' => $this->numericMessage('収納時の奥行き'),
+            'baseData.storage_height.required' => $this->requiredMessage('収納時の高さ'),
+            'baseData.storage_height.numeric' => $this->numericMessage('収納時の高さ'),
+            'baseData.weight.required' => $this->requiredMessage('重量'),
+            'baseData.weight.numeric' => $this->numericMessage('重量'),
+            'baseData.brand_name.required' => $this->requiredMessage('ブランド名'),
+            'baseData.brand_name.string' => $this->stringMessage('画像名'),
             'baseData.brand_name.max' => 'ブランド名は最大50文字までです。',
-            'baseData.brand_name.string' => 'ブランド名は文字列である必要があります。',
-            'baseData.item_category_name.required' => '商品カテゴリー名は必須です。',
-            'baseData.item_category_name.string' => '商品カテゴリー名は文字列である必要があります。',
+            'baseData.item_category_name.required' => $this->requiredMessage('商品カテゴリー名'),
+            'baseData.item_category_name.string' => $this->stringMessage('商品カテゴリー名'),
             'baseData.item_category_name.max' => '商品カテゴリー名は最大50文字までです。',
-            'baseData.sub_category_name.required' => 'サブカテゴリー名は必須です。',
-            'baseData.sub_category_name.string' => 'サブカテゴリー名は文字列である必要があります。',
+            'baseData.sub_category_name.required' => $this->requiredMessage('サブカテゴリー名'),
+            'baseData.sub_category_name.string' => $this->stringMessage('サブカテゴリー名'),
             'baseData.sub_category_name.max' => 'サブカテゴリー名は最大50文字までです。',
             'itemTags.*.max' => 'アイテムタグは最大50文字までです。',
-            'itemTags.*.string' => 'アイテムタグは文字列である必要があります。',
-            'colorTags.required' => 'カラータグは必須です。',
+            'itemTags.*.string' => $this->stringMessage('アイテムタグ'),
+            'colorTags.required' => $this->requiredMessage('カラータグ'),
             'colorTags.*.max' => 'カラータグは最大20文字までです。',
-            'colorTags.*.string' => 'カラータグは文字列である必要があります。',
-            'details.capacity.numeric' => '容量は数値である必要があります。',
+            'colorTags.*.string' => $this->stringMessage('カラータグ'),
+            'details.capacity.numeric' => $this->numericMessage('容量'),
             'details.inner_tent.max' => 'インナーテントの詳細は最大255文字までです。',
-            'details.inner_tent.string' => 'インナーテントの詳細は文字列である必要があります。',
+            'details.inner_tent.string' => $this->stringMessage('インナーテントの詳細'),
             'details.grand_sheet.max' => 'グランドシートの詳細は最大255文字までです。',
-            'details.grand_sheet.string' => 'グランドシートの詳細は文字列である必要があります。',
+            'details.grand_sheet.string' => $this->stringMessage('グランドシートの詳細'),
             'details.fabrics.max' => '素材の詳細は最大255文字までです。',
-            'details.fabrics.string' => '素材の詳細は文字列である必要があります。'
+            'details.fabrics.string' => $this->stringMessage('素材の詳細')
         ];
+    }
+    private function stringMessage($field)
+    {
+        return "{$field}は文字列である必要があります。";
+    }
+    private function numericMessage($field)
+    {
+        return "{$field}は数値である必要があります。";
+    }
+    private function requiredMessage($field)
+    {
+        return "{$field}は必須です。";
     }
 }

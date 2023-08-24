8362ac817aebfba09b416277b2e769877b9209e3
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-25 00:47:45 +0900

    ルート定義の整理

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,55 +3,35 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ItemController;
-use App\Http\Controllers\UserInventoryController;
-use App\Http\Controllers\FavoriteItemController;
-use App\Http\Controllers\FavoriteLayoutController;
 use App\Http\Controllers\PrivateLayoutController;
 use App\Http\Controllers\PublicLayoutController;
+use App\Http\Controllers\FavoriteItemController;
+use App\Http\Controllers\FavoriteLayoutController;
+use App\Http\Controllers\UserInventoryController;
 
-// ユーザー関連のルート
-Route::prefix('user')->group(function () {
-    // 認証が不要なユーザー関連のルート
-    Route::post('register', [UserController::class, 'register']);
-    Route::post('login', [UserController::class, 'login']);
-
-    // 認証が必要なユーザー関連のルート
-    Route::middleware(['auth:api'])->group(function () {
-        Route::post('logout', [UserController::class, 'logout']);
-        Route::post('update', [UserController::class, 'update']);
-        Route::post('delete', [UserController::class, 'delete']);
-    });
-});
-
-// パブリックなレイアウト関連のルート
-Route::get('layout', [PublicLayoutController::class, 'index']);
-Route::get('layout/{id}', [PublicLayoutController::class, 'show'])
-    ->middleware('AttachUserIdToRequest');
-
-
-// アイテム関連のルート
+// 認証が不要なルート
+Route::post('user/register', [UserController::class, 'register']);
+Route::post('user/login', [UserController::class, 'login']);
 Route::apiResource('items', ItemController::class)->only(['index', 'store', 'update', 'destroy']);
-Route::get('items/{id}', [ItemController::class, 'show'])
-    ->middleware('AttachUserIdToRequest');
+Route::get('items/{item}', [ItemController::class, 'show'])->middleware('AttachUserIdToRequest');
+Route::get('layout', [PublicLayoutController::class, 'index']);
+Route::get('layout/{layout}', [PublicLayoutController::class, 'show'])->middleware('AttachUserIdToRequest');
 
 // 認証が必要なルート
 Route::middleware(['auth:api'])->group(function () {
-    // ユーザーのレイアウト関連のルート
-    Route::apiResource('user/layout', PrivateLayoutController::class);
-
-    // ユーザーのインベントリ関連のルート
-    Route::apiResource('user/inventory', UserInventoryController::class)
-        ->only(['index']);
-
-    // ユーザーのお気に入りアイテム関連のルート
-    Route::apiResource('user/favorite/items', FavoriteItemController::class)
-        ->only(['index']);
-    // ユーザーのお気に入りレイアウト関連のルート
-    Route::apiResource('user/favorite/layouts', FavoriteLayoutController::class)
-        ->only(['index', 'destroy']);
-    Route::post('user/favorite/layouts/{layout}', [FavoriteLayoutController::class, 'store']);
-    Route::post('user/inventory/{item}', [UserInventoryController::class, 'store']);
-    Route::delete('user/inventory/{item}', [UserInventoryController::class, 'destroy']);
-    Route::post('user/favorite/items/{item}', [FavoriteItemController::class, 'store']);
-    Route::delete('user/favorite/items/{item}', [FavoriteItemController::class, 'destroy']);
+    // ユーザー関連のルート
+    Route::prefix('user')->group(function () {
+        Route::post('logout', [UserController::class, 'logout']);
+        Route::post('update', [UserController::class, 'update']);
+        Route::post('delete', [UserController::class, 'delete']);
+        Route::apiResource('layout', PrivateLayoutController::class);
+        Route::apiResource('inventory', UserInventoryController::class)->only(['index']);
+        Route::apiResource('favorite/items', FavoriteItemController::class)->only(['index']);
+        Route::apiResource('favorite/layouts', FavoriteLayoutController::class)->only(['index', 'destroy']);
+        Route::post('favorite/layouts/{layout}', [FavoriteLayoutController::class, 'store']);
+        Route::post('inventory/{item}', [UserInventoryController::class, 'store']);
+        Route::delete('inventory/{item}', [UserInventoryController::class, 'destroy']);
+        Route::post('favorite/items/{item}', [FavoriteItemController::class, 'store']);
+        Route::delete('favorite/items/{item}', [FavoriteItemController::class, 'destroy']);
+    });
 });

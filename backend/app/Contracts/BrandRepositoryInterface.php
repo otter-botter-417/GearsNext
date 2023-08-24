c325839f0824e65218a25ab4463ce1a4053d2c1f
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-25 00:45:50 +0900

    Docの追加

--- backend/app/Contracts/BrandRepositoryInterface.php
+++ backend/app/Contracts/BrandRepositoryInterface.php
@@ -2,9 +2,22 @@
 
 namespace App\Contracts;
 
+use App\Models\Brand;
+
 interface BrandRepositoryInterface
 {
-    public function getAll();
-    public function find($id);
-    public function getBrandByNameOrThrow($brandId);
+    /**
+     * ブランドを取得
+     * @param  int  $brandId
+     * @return Brand|null
+     */
+    public function find(int $brandId): ?Brand;
+
+    /**
+     * ブランド名からブランドを取得
+     * @param  string $brandName
+     * @throws BrandNotFoundException ブランドが見つからない場合
+     * @return Brand 
+     */
+    public function getBrandByName(string $brandName): Brand;
 }

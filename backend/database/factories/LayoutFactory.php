c665d8081b67e177bac44794b0134aba3a7b5a19
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-26 13:07:35 +0900

    LayoutFactoryを実装

--- backend/database/factories/LayoutFactory.php
+++ backend/database/factories/LayoutFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Item;
+use App\Models\Layout;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,37 @@ class LayoutFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'text' => $this->faker->realText(50),
+            'user_id' => User::inRandomOrder()->first()->user_id,
+            'favorite_count' => $this->faker->numberBetween(0, 1000),
+            'view_count' => $this->faker->numberBetween(0, 5000),
+
         ];
+    }
+
+    /**
+     * リレーションを考慮したデータを作成
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function createWithRelations()
+    {
+        return $this->afterCreating(function (Layout $layout) {
+            // Tagからランダムに1個のIDを取得
+            $randomItem = Item::inRandomOrder()->first();
+            if ($randomItem) {
+                $items = [
+                    [
+                        'item_id' => $randomItem->item_id,
+                        'x_position' => $this->faker->randomFloat(1, 10, 100),
+                        'y_position' => $this->faker->randomFloat(1, 10, 100),
+                    ]
+                    // 他のアイテムもこの形式で追加できます
+                ];
+
+                // 新しく作成されたLayoutに対して、取得したItemのIDを紐付ける
+                $layout->tagPositions()->createMany($items);
+            }
+        })->create();
     }
 }

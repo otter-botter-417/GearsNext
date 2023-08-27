15b923badcf1f61ad4272e0afe8f6a785edd5097
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-08-28 00:12:36 +0900

    indexメソッドのリターンを、LayoutResourceからLayoutIndexResourceに変更

--- backend/app/Http/Controllers/PrivateLayoutController.php
+++ backend/app/Http/Controllers/PrivateLayoutController.php
@@ -6,7 +6,7 @@ use App\Models\Layout;
 use App\Services\LayoutService;
 use App\Http\Requests\StoreLayoutRequest;
 use App\Http\Requests\UpdateLayoutRequest;
-use App\Http\Resources\LayoutResource;
+use App\Http\Resources\LayoutIndexResource;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Response;
 use Illuminate\Http\Resources\Json\ResourceCollection;
@@ -32,7 +32,7 @@ class PrivateLayoutController extends Controller
     public function index(): ResourceCollection
     {
         $layouts = $this->layoutService->getLayouts(Auth::id());
-        return LayoutResource::collection($layouts);
+        return LayoutIndexResource::collection($layouts);
     }
 
     /**

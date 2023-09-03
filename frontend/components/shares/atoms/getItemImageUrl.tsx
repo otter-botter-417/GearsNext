e9af76c961ab32b7947449d7c50fb6c4a21d4fd8
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-04 01:02:53 +0900

    引数のimage_nameをimageNameに変更

--- frontend/components/shares/atoms/getItemImageUrl.tsx
+++ frontend/components/shares/atoms/getItemImageUrl.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 /**
  * 商品画像のURLを取得する
  * @param brandName ブランド名
- * @param image_name 画像名
+ * @param imageName 画像名
  * @returns 商品画像のURL
  * @example
- * const ItemImagesUrl = getItemImageUrl(ItemData.brand_name, ItemData.image_name);
+ * const ItemImagesUrl = getItemImageUrl(ItemData.brand_name, ItemData.imageName);
  */
 export const getItemImageUrl = (
   brandName: string,
-  image_name: string,
+  imageName: string,
 ): string => {
   return (
     'https://gears-item-images.s3.ap-northeast-1.amazonaws.com/items/' +
     brandName +
     '/' +
-    image_name +
+    imageName +
     '.jpg'
   );
 };

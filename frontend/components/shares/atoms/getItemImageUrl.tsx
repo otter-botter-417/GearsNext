6c15c051c6ff2a5077e77440de005c44d39072fc
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-04 01:00:26 +0900

    Docを追記

--- frontend/components/shares/atoms/getItemImageUrl.tsx
+++ frontend/components/shares/atoms/getItemImageUrl.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
 
+/**
+ * 商品画像のURLを取得する
+ * @param brand ブランド名
+ * @param image_name 画像名
+ * @returns 商品画像のURL
+ * @example
+ * const ItemImagesUrl = getItemImageUrl(ItemData.brand_name, ItemData.image_name);
+ */
 export const getItemImageUrl = (brand: string, image_name: string): string => {
   return (
-    "https://gears-item-images.s3.ap-northeast-1.amazonaws.com/items/" +
+    'https://gears-item-images.s3.ap-northeast-1.amazonaws.com/items/' +
     brand +
-    "/" +
+    '/' +
     image_name +
-    ".jpg"
+    '.jpg'
   );
 };
 

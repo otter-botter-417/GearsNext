338febc622b6f0d6768a76d9ed4b67519c316b97
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-12 02:38:25 +0900

    ページネーションを実装

--- frontend/components/shares/organisms/ItemThumbnailGrid.tsx
+++ frontend/components/shares/organisms/ItemThumbnailGrid.tsx
@@ -5,9 +5,13 @@ import Grid from '@mui/material/Grid';
 import { ItemDataTypes } from '@/components/types/ItemDataTypes';
 import { ItemThumbnail } from '@/components/shares/molecules/ItemThumbnail';
 import { filteredItemsState } from '@/components/shares/atoms/state/filteredItemsState';
+import { paginationState } from '../atoms/state/paginationState';
+import { PaginationControls } from '../molecules/PaginationControls';
+import { Box } from '@mui/system';
 
 /**
  * 商品のサムネイル画像と基本情報を並べたグリッド
+ * ページネーション機能付き
  *
  * @returns 商品のサムネイルを並べたグリッド
  * @example
@@ -15,22 +19,36 @@ import { filteredItemsState } from '@/components/shares/atoms/state/filteredItem
  */
 export const ItemThumbnailGrid = () => {
   const filteredItemList = useRecoilValue(filteredItemsState);
+
+  // ページネーションの設定
+  const pagination = useRecoilValue(paginationState);
+  const startIndex = (pagination.currentPage - 1) * pagination.itemsPerPage;
+  const endIndex = startIndex + pagination.itemsPerPage;
+  const currentItems = filteredItemList?.slice(startIndex, endIndex);
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{ position: 'center' }}
-      style={{ width: '80%', margin: '0 auto' }}
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      alignItems="center"
     >
-      {filteredItemList ? (
-        filteredItemList.map((data: ItemDataTypes, index: number) => (
-          <Grid item xs={5} sm={4} md={3} key={index}>
-            <ItemThumbnail ItemData={data} key={`item-${index}`} />
-          </Grid>
-        ))
-      ) : (
-        <p>No items found</p>
-      )}
-    </Grid>
+      <PaginationControls />
+      <Grid
+        container
+        spacing={2}
+        sx={{ position: 'center' }}
+        style={{ width: '80%', margin: '0 auto' }}
+      >
+        {currentItems ? (
+          currentItems.map((data: ItemDataTypes, index: number) => (
+            <Grid item xs={5} sm={4} md={3} key={index}>
+              <ItemThumbnail ItemData={data} key={`item-${index}`} />
+            </Grid>
+          ))
+        ) : (
+          <p>No items found</p>
+        )}
+      </Grid>
+      <PaginationControls />
+    </Box>
   );
 };

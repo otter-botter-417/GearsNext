8aa0c479817755f1b32a944642065472c481d638
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-11 22:43:50 +0900

    コンポーネントが複雑化してしまっていたので依存をすくなくした

--- frontend/components/pages/ItemPage/FavoriteIconButton.tsx
+++ frontend/components/pages/ItemPage/FavoriteIconButton.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import { useFavoriteItemApi } from '@/hooks/api/useFavoriteItemApi';
 
 import { UserInteractiveIconButton } from '../../shares/molecules/UserInteractiveIconButton';
-import { useInteractiveIcon } from '@/hooks/useInteractiveIcon';
-import { ICON_SIZE } from '@/components/constants';
+import {
+  ICON_SIZE,
+  LOGIN_ALERT_TIMEOUT,
+  STATUS_CREATED,
+  STATUS_NO_CONTENT,
+} from '@/components/constants';
+import { useTimedToggle } from '@/hooks/useTimedToggle';
 
 type FavoriteIconButtonProps = {
   itemId: number;
@@ -20,26 +25,22 @@ type FavoriteIconButtonProps = {
  * ログイン済みの場合はいいねの登録・解除を行う
  *
  * @param itemId
- * @param isLoggedIn
- * @param isFavorited
+ * @param isLoggedIn　ログイン状態
+ * @param isFavorited　いいね状態
  */
-
-const ICON_STYLE_ACTIVE = { color: '#FF1744', fontSize: ICON_SIZE };
-const ICON_STYLE_INACTIVE = { fontSize: ICON_SIZE };
-
 export const FavoriteIconButton: FC<FavoriteIconButtonProps> = ({
   itemId,
   isLoggedIn,
   isFavorited,
 }) => {
   const { sendFavoriteItemRequest } = useFavoriteItemApi();
-
-  const { isIconActive, showLoginAlert, onClickIcon } = useInteractiveIcon({
-    initialState: isFavorited,
-    sendRequest: sendFavoriteItemRequest,
-    itemId,
-    isLoggedIn,
-  });
+  const [isIconActive, setIsIconActive] = useState(isFavorited);
+  const [showLoginAlert, setShowLoginAlert] =
+    useTimedToggle(LOGIN_ALERT_TIMEOUT);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const ICON_STYLE_ACTIVE = { color: '#FF1744', fontSize: ICON_SIZE };
+  const ICON_STYLE_INACTIVE = { fontSize: ICON_SIZE };
 
   /**
    * いいねアイコンをクリックした時の処理
@@ -48,6 +49,35 @@ export const FavoriteIconButton: FC<FavoriteIconButtonProps> = ({
    * レスポンスを待たずにstateの切り替えを行う
    * レスポンスがエラーの場合はstateを元に戻す
    */
+  const onClickIcon = async () => {
+    // 未ログインの場合はログインアラートを表示する
+    if (!isLoggedIn) {
+      setShowLoginAlert();
+      return;
+    }
+    // ボタンが押された時に連続でリクエストが送られないようにする
+    if (isLoading) return;
+
+    setIsLoading(true);
+    setIsIconActive((isIconActive) => !isIconActive);
+    try {
+      if (isIconActive) {
+        await sendFavoriteItemRequest('delete', itemId, STATUS_NO_CONTENT);
+      } else {
+        await sendFavoriteItemRequest('post', itemId, STATUS_CREATED);
+      }
+    } catch (error) {
+      // エラーが発生した場合はstateを元に戻す
+      alert(error);
+      setIsIconActive((isIconActive) => !isIconActive);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setIsIconActive(isFavorited);
+  }, [isFavorited]);
 
   return (
     <UserInteractiveIconButton

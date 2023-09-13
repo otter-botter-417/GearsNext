557e40c0f7aeb84da86edebfcfa2eee40460e2c3
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-13 15:58:29 +0900

    itemテーブルのリレーションを修正

--- backend/app/Models/TagPosition.php
+++ backend/app/Models/TagPosition.php
@@ -36,6 +36,6 @@ class TagPosition extends Model
 
     public function item()
     {
-        return $this->belongsTo(Item::class);
+        return $this->belongsTo(Item::class, 'item_id');
     }
 }

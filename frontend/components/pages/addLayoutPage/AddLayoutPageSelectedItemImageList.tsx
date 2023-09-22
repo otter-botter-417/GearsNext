faa139466ea0b20f6d39b8a39691973fab14a5c8
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-23 02:07:23 +0900

    レイアウト投稿ページで、選択された商品画像を、グリッド表示するコンポーネントを実装しました。

---
+++ frontend/components/pages/addLayoutPage/AddLayoutPageSelectedItemImageList.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import { useRecoilValue } from 'recoil';
+import { Box } from '@mui/material';
+
+import ImageGridList from '@/components/shares/molecules/ImageGridList';
+import { selectedItemsListState } from '@/components/shares/atoms/state/selectedItemsListState';
+
+/**
+ * このコンポーネントは、レイアウト投稿ページで選択された商品の画像を表示するためのUIを提供します。
+ * - ImageGridList: selectedItemsListState ステートから取得した商品の画像を表示します。
+ *
+ * @returns {JSX.Element} 選択された商品の画像を表示するコンポーネントを含むReact要素
+ */
+export const AddLayoutPageSelectedItemImageList: FC = () => {
+  const selectedItemsList = useRecoilValue(selectedItemsListState);
+
+  return (
+    <Box
+      sx={{
+        overflowY: 'scroll',
+        height: '100',
+        width: '100%',
+      }}
+    >
+      <ImageGridList itemList={selectedItemsList} imageSize={100} cols={5} />
+    </Box>
+  );
+};

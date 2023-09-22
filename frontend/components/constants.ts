12efc49bd6c718594bdc133ded70990415e611f0
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-23 01:47:16 +0900

    全体で利用する定数を定義するコンポーネントを実装しました。。

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -1,6 +1,12 @@
+export const STATUS_OK = 200;
 export const STATUS_CREATED = 201;
 export const STATUS_NO_CONTENT = 204;
 export const LOGIN_ALERT_TIMEOUT = 2000;
 export const ICON_SIZE = 30;
 export const BASE_URL = 'http://localhost:3000';
-export const LAYOUT_IMAGE_BASE_URL = 'https://gears-item-images.s3.ap-northeast-1.amazonaws.com/layouts/';
+// export const API_BASE_URL = 'http://54.168.247.147:8000/api/' //aws
+export const API_BASE_URL = 'http://127.0.0.1:8000/api/' //local
+export const ITEM_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/item/';
+export const LAYOUT_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/layout/image/';
+
+

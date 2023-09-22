eadfd22c0b02dfd6f71431fa63efb6fbb9b7345e
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-23 01:54:09 +0900

    ユーザーに選択されたイメージファイルを管理するrecoilStateを実装しました。

---
+++ frontend/components/shares/atoms/state/imageFileState.ts
@@ -0,0 +1,10 @@
+import { atom, } from 'recoil';
+
+/**
+ * イメージファイルを管理するrecoil State
+ * nullの場合、イメージファイルが選択されていないことを示す。
+ */
+export const imageFileState = atom<File | null>({
+  key: 'imageFileState',
+  default: null,
+});

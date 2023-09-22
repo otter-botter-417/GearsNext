5f03eb0e0cc1f61e9917980a42fb6f42413a0bb7
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-23 01:54:44 +0900

    Docコメントをわかりやすく修正しました。

--- frontend/components/shares/atoms/state/imageFileState.ts
+++ frontend/components/shares/atoms/state/imageFileState.ts
@@ -1,7 +1,7 @@
 import { atom, } from 'recoil';
 
 /**
- * イメージファイルを管理するrecoil State
+ * ユーザーに選択されたイメージファイルを管理するrecoil State
  * nullの場合、イメージファイルが選択されていないことを示す。
  */
 export const imageFileState = atom<File | null>({

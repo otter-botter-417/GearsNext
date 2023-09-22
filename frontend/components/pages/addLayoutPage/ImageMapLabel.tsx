a945538772bfffadce74b9d3d9efc466f6c1c163
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-23 02:11:52 +0900

    レイアウト投稿ページの、選択されたイメージマップを表示するコンポーネントを実装しました。

---
+++ frontend/components/pages/addLayoutPage/ImageMapLabel.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+
+import { LabelItem } from './LabelItem';
+import { imageMapDataListState } from '@/components/shares/atoms/state/imageMapDataListState';
+
+/**
+ * このコンポーネントは、imageMapDataList ステートから取得した情報を用いて、
+ * 選択済みのイメージマップラベルを画面に表示します。
+ *
+ * @returns {JSX.Element} 選択されたイメージマップのラベル一覧
+ *
+ * @example
+ * <ImageMapLabel />
+ */
+export const ImageMapLabel = () => {
+  const [imageMapDataList, setImageMapDataList] = useRecoilState(
+    imageMapDataListState,
+  );
+
+  /**
+   * イメージマップから指定の要素（アイテムIDによって識別）を削除する
+   * @param itemId
+   */
+  const removeLabel = (itemId: number): void => {
+    setImageMapDataList((prevList) =>
+      prevList.filter((item) => item.itemId !== itemId),
+    );
+  };
+
+  if (!imageMapDataList) return null;
+
+  return (
+    <>
+      {imageMapDataList.map((item) => (
+        <LabelItem key={item.itemId} item={item} onRemove={removeLabel} />
+      ))}
+    </>
+  );
+};

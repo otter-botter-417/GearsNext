707cdaa4a5b850cc556517dd45b3f1248ce0dde3
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-27 15:12:39 +0900

    jwtのaccessトークンが認証切れだった場合、refreshトークンを利用してアクセストークンを更新するように変更。

--- frontend/hooks/api/useApiRequest.ts
+++ frontend/hooks/api/useApiRequest.ts
@@ -1,52 +1,83 @@
-import { API_BASE_URL } from "@/components/constants";
-import axios, { isAxiosError } from "axios";
-
 import { useCallback } from "react";
+import axios, { AxiosResponse, isAxiosError } from "axios";
+
+import { API_BASE_URL } from "@/components/constants";
 
 /**
- * バックエンドへAPIリクエストを送信し、レスポンスを受け取る
- * @returns {sendRequest} APIリクエストを送信する関数
+ * バックエンドへAPIリクエストを送信し、レスポンスを受け取るカスタムフック。
+ * @returns {sendRequest} APIリクエストを送信する関数。
+ * 
  * @example
  * const { sendRequest } = useApiRequest();
  * const response = await sendRequest('GET', 'items', null);
  */
 export const useApiRequest = () => {
-
   /**
-   * APIリクエストを送信する関数
-   * @param method リクエストメソッド (GET, POST, PUT, DELETE)
-   * @param url リクエストURL api/以下のパス
-   * @param requestData リクエストデータ
-   * @returns APIレスポンス
-   * @example
-   * const { sendRequest } = useApiRequest();
-   * const response = await sendRequest('GET', 'items', null);
+   * アクセストークンをリフレッシュする。
+   * @returns 新しいアクセストークンまたはnull。　
    */
-  const sendRequest = useCallback(async (method: string, url: string, requestData: any = null) => {
-    const token = localStorage.getItem('jwt_token');
-    const headers: { [key: string]: string } = {};
-
-    if (token) {
-      headers['Authorization'] = `Bearer ${token}`;
-    }
-
+  const refreshAccessToken = async () => {
+    const refreshToken = localStorage.getItem('refresh_token');
     try {
-      const response = await axios.request({
-        method,
-        url: API_BASE_URL + url,
-        data: requestData,
-        headers
+      const response = await axios.post(API_BASE_URL + 'user/refresh', null, {
+        headers: {
+          'Authorization': `Bearer ${refreshToken}`
+        }
       });
-      return response;
+      const newAccessToken = response.data.access_token;
+      localStorage.setItem('access_token', newAccessToken);
+      return newAccessToken;
     } catch (err) {
-      if (isAxiosError(err)) {
-        throw err.response;
-      }
-      //未知のエラーの場合はnullを返す
-      console.error(err);
+      console.error("Failed to refresh token", err);
       return null;
     }
-  }, []);
+  };
 
+  /**
+   * 実際にAPIリクエストを実行する内部関数。
+   */
+  const performRequest = async (method: string, url: string, requestData: any, token: string | null) => {
+    const headers: { [key: string]: string } = {};
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+    return await axios.request({
+      method,
+      url: API_BASE_URL + url,
+      data: requestData,
+      headers,
+    });
+  };
+
+  /**
+  * APIリクエストを送信する関数
+  * - トークンが期限切れの場合は、トークンをリフレッシュしてからリクエストを再送信する。
+  * @param method リクエストメソッド (GET, POST, PUT, DELETE)
+  * @param url リクエストURL api/以下のパス
+  * @param requestData リクエストデータ
+  * @returns APIレスポンス
+  * @example
+  * const { sendRequest } = useApiRequest();
+  * const response = await sendRequest('GET', 'items', null);
+  */
+  const sendRequest = useCallback(
+    async (method: string, url: string, requestData: any = null): Promise<AxiosResponse | null> => {
+      let token = localStorage.getItem("access_token");
+      try {
+        const response = await performRequest(method, url, requestData, token);
+        return response;
+      } catch (err) {
+        if (isAxiosError(err) && err.response?.status === 401) {
+          const newToken = await refreshAccessToken();
+          if (newToken) {
+            return await performRequest(method, url, requestData, newToken);
+          }
+        }
+        console.error("APIリクエストに失敗しました。", err);
+        return null;
+      }
+    },
+    []
+  );
   return { sendRequest };
 };

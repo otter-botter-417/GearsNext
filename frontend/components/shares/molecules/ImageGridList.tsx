aeaffc06f7270cf3d7cc384f0b57f0cf4d5198bd
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-27 16:09:09 +0900

    Imageに関する処理を別コンポーネントに分割し、画像をクリックで商品ページに遷移するように変更しました。。

--- frontend/components/shares/molecules/ImageGridList.tsx
+++ frontend/components/shares/molecules/ImageGridList.tsx
@@ -1,52 +1,53 @@
 import React, { FC } from 'react';
-import Image from 'next/image';
 import { ImageList, ImageListItem } from '@mui/material';
 
-import { ITEM_IMAGE_BASE_URL } from '@/components/constants';
-import { ItemDataTypes } from '@/components/types/ItemDataTypes';
+import Link from 'next/link';
+import { ItemImage } from '../atoms/ItemImage';
 
 type ImageGridListProps = {
-  itemList: ItemDataTypes[];
+  itemList: {
+    itemId: number;
+    imageName: string;
+  }[];
+
   imageSize: number;
   cols: number;
+  isLink?: boolean;
 };
 
 /**
  * このコンポーネントは、受け取った商品リストから、画像をグリッドで表示するUIを提供します。
  *
- * @param {ItemDataTypes[]} itemList - 表示する商品のリスト
+ * @param {ItemDataType[]} itemList - 表示する商品のリスト
  * @param {number} imageSize - 画像のサイズ（縦・横は同じサイズ）
  * @param {number} cols - 一行に表示する画像の数
+ * @param {boolean} isLink - 商品画像をクリックしたときに、商品詳細ページに遷移するかどうか
  *
  * @returns {JSX.Element} 商品画像をグリッドで表示するコンポーネントを含むReact要素
  *
  * @example
  * <ImageGridList itemList={itemList} imageSize={100} cols={5} />
  */
-const ImageGridList: FC<ImageGridListProps> = ({
+export const ImageGridList: FC<ImageGridListProps> = ({
   itemList,
   imageSize,
   cols,
+  isLink = false,
 }) => {
   return (
     <ImageList sx={{ width: 'auto', height: 'auto' }} cols={cols}>
       {itemList.map((item) => (
         <ImageListItem key={item.itemId}>
-          <Image
-            src={`${ITEM_IMAGE_BASE_URL}${item.itemId}.jpg`}
-            alt={`${ITEM_IMAGE_BASE_URL}${item.itemId}.jpg`}
-            layout="responsive"
-            width={imageSize}
-            height={imageSize}
-            loading="lazy"
-            style={{
-              objectFit: 'cover',
-            }}
-          />
+          {/* isLink が true の場合、画像を Link コンポーネントで囲む */}
+          {isLink ? (
+            <Link href={`/items/${item.itemId}`}>
+              <ItemImage item={item} imageSize={imageSize} />
+            </Link>
+          ) : (
+            <ItemImage item={item} imageSize={imageSize} />
+          )}
         </ImageListItem>
       ))}
     </ImageList>
   );
 };
-
-export default ImageGridList;

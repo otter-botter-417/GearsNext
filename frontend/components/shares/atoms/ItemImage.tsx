415b7f961ce16e29abec6de0926461a4dd7710d7
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-27 20:02:46 +0900

    軽微な修正。

--- frontend/components/shares/atoms/ItemImage.tsx
+++ frontend/components/shares/atoms/ItemImage.tsx
@@ -10,6 +10,13 @@ type ItemImageProps = {
   imageSize: number;
 };
 
+/**
+ * コンポーネントは、商品の画像を表示します
+ *
+ * @param item - 商品情報。itemIdとimageNameが含まれる。
+ * @param imageSize - 表示する画像のサイズ
+ * @returns {JSX.Element} 商品の画像
+ */
 export const ItemImage: FC<ItemImageProps> = ({ item, imageSize }) => {
   console.log(item);
   return (

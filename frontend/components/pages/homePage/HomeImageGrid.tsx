a139c74352d54078cedc9b7ceb74623bc2df93f4
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-01 04:27:55 +0900

    Homeで画像をグリッド表示をするコンポーネントを実装しました。

---
+++ frontend/components/pages/homePage/HomeImageGrid.tsx
@@ -0,0 +1,32 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { SortSelect } from './SortSelect';
+
+export const HomeImageGrid = () => {
+  return (
+    <Box>
+      <Box display="flex" justifyContent={'center'} >
+        <Typography variant="h5">商品ランキング</Typography>
+      </Box>
+      <Box height="40vh" justifyContent={'center'}>
+        <SortSelect
+          initialState="topViewedItems"
+          tabKeys={['topViewedItems', 'topFavoriteItems', 'newlyArrivedItems']}
+        />
+      </Box>
+      <Box display="flex" justifyContent={'center'}>
+        <Typography variant="h5">レイアウトランキング</Typography>
+      </Box>
+      <Box height="40vh" justifyContent={'center'}>
+        <SortSelect
+          initialState="topViewedLayouts"
+          tabKeys={[
+            'topViewedLayouts',
+            'topFavoriteLayouts',
+            'newlyArrivedLayouts',
+          ]}
+        />
+      </Box>
+    </Box>
+  );
+};

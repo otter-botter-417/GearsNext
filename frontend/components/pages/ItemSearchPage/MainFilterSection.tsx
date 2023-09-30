457f555d7ece1b367934da7a670b6b37adb54002
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-01 04:22:22 +0900

    検索ページの主要なフィルタリングオプションをまとめたコンポーネントを実装しました。

---
+++ frontend/components/pages/ItemSearchPage/MainFilterSection.tsx
@@ -0,0 +1,46 @@
+import { Grid } from '@mui/material';
+
+import { CategoryNameList } from '@/components/shares/atoms/SelectNames/CategoryNameList';
+
+import { categoryValueState } from '@/components/shares/atoms/state/categoryValueState';
+
+import { PullDownSelector } from '@/components/shares/molecules/PullDownSelector';
+import { SearchBar } from '@/components/pages/ItemSearchPage/SearchBar';
+import { SubCategorySelector } from './SubCategorySelector';
+
+/**
+ * このコンポーネントは商品検索ページの主要なフィルタリングオプションを提供するセクションです。
+ * - キーワードで検索するための検索バー
+ * - メインカテゴリーを選択するためのドロップダウンメニュー
+ * - 選択されたメインカテゴリーに基づいて変更されるサブカテゴリーセレクター
+ *
+ * @example
+ * ```tsx
+ * <MainFilterSection />
+ * ```
+ *
+ * @returns 商品検索ページの主要なフィルタリングオプションを提供するセクション。
+ */
+export const MainFilterSection = () => {
+  const ExtendedCategoryNameList = ['すべてのカテゴリー', ...CategoryNameList];
+  return (
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={12}>
+        <SearchBar />
+      </Grid>
+
+      {/* カテゴリーとサブカテゴリー */}
+      <Grid item xs={12} sm={6}>
+        <PullDownSelector
+          options={ExtendedCategoryNameList}
+          label="Category"
+          stateAtom={categoryValueState}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        {/* サブカテゴリー切り替え */}
+        <SubCategorySelector />
+      </Grid>
+    </Grid>
+  );
+};

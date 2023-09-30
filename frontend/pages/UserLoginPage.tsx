c3e0480ab3944373042b4032b4aaff4858469d4e
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 21:10:35 +0900

    テンプレートを修正し、各propsに渡すようリファクタリングしました。

--- frontend/pages/UserLoginPage.tsx
+++ frontend/pages/UserLoginPage.tsx
@@ -1,41 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Typography } from '@mui/material';
+import React from 'react';
+import Link from '@mui/material/Link';
+import { Typography, Box } from '@mui/material';
 
+import { AuthPageTemplate } from '@/components/templates/AuthPageTemplate';
 import { useUserLogin } from '@/hooks/UserAuth/useUserLogin';
-import { FormMethodsProvider } from '@/hooks/useFormMethods ';
-
-import { SubmitButton } from '@/components/shares/atoms/SubmitButton';
-import RegisterPageTemplate from '@/components/templates/RegisterPageTemplate';
-import LoginForm from '@/components/pages/userLoginPage/LoginForm';
+import { useRecoilValue } from 'recoil';
+import { userState } from '@/components/shares/atoms/state/userState';
+import { useRouter } from 'next/router';
+import { LoginForm } from '@/components/pages/userLoginPage/LoginForm';
 
 /**
- * ユーザーのログインページ
+ * ユーザーログインページ
+ *
+ * @remarks
+ * このページには以下の要素が含まれます：
+ * - タイトル ("ログイン")
+ * - ログインフォーム
+ * - 新規登録ページへのリンク
+ *
+ * @returns ユーザーログインページの要素
  */
 const UserLoginPage = () => {
   const { formMethods, onSubmit } = useUserLogin();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const user = useRecoilValue(userState);
+  const router = useRouter();
 
-  useEffect(() => {
-    setIsAuthenticated(!!localStorage.getItem('jwt_token'));
-  }, []);
+  // 既にログインしている場合はホームページにリダイレクト
+  if (user) {
+    router.push('/');
+  }
 
-  return (
-    <RegisterPageTemplate>
-      <FormMethodsProvider formMethods={formMethods}>
-        <Typography variant="h4">ログイン</Typography>
-        {isAuthenticated ? <p>ログアウト</p> : <p>ログインしてください</p>}
-        <form onSubmit={formMethods.handleSubmit(onSubmit)}>
-          <LoginForm formMethods={formMethods} />
-          <SubmitButton
-            loading={formMethods.watch('loading') || false}
-            text={'ログイン'}
-          />
-        </form>
-        <Link href="/UserRegisterPage">アカウントを持っていない</Link>
-      </FormMethodsProvider>
-    </RegisterPageTemplate>
-  );
+  const title = <Typography variant="h4">ログイン</Typography>;
+  const loginForm = <LoginForm />;
+  const link = <Link href="/UserRegisterPage">アカウントを持っていない</Link>;
+
+  return <AuthPageTemplate title={title} form={loginForm} link={link} />;
 };
 
 export default UserLoginPage;

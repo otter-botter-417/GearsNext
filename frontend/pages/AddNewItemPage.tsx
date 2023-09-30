fe36c6c2e7c9dbab13f5151aba57a7f025bd6d80
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-01 03:25:07 +0900

    fromMethodsに関する処理をuseAddNewFormに移動しました。

--- frontend/pages/AddNewItemPage.tsx
+++ frontend/pages/AddNewItemPage.tsx
@@ -1,73 +1,38 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useItemForm } from '../hooks/useItemFormToLaravel'; // mongoDBに商品データを送信するためのカスタムフック
-import { SubmitButton } from '@/components/shares/atoms/SubmitButton';
-import AddNewItemPageTemplate from '@/components/templates/AddNewItemPageTemplate';
-import BaseItemDataForm from '@/components/shares/organisms/BaseItemDataForm';
-import CategoryDetailList from '@/components/shares/molecules/CategoryDetailList';
-import { useDetailFormMethods } from '@/hooks/useDetailFormMethods';
-import { AddNewItemValidatedSchema } from '@/components/shares/atoms/schema/AddNewItemValidatedSchema';
 
-// 新規に商品情報をmongoDBに送信、登録するページ
+import { FormProvider } from '@/hooks/useFormMethods ';
+
+import { AddNewItemPageTemplate } from '@/components/templates/AddNewItemPageTemplate';
+import { BaseItemDataForm } from '@/components/pages/addItemPage/BaseItemDataForm';
+import { CategoryDetailList } from '@/components/pages/addItemPage/CategoryDetailList';
+import { SubmitButton } from '@/components/shares/atoms/button/SubmitButton';
+import { useAddNewItemForm } from '@/hooks/useAddNewItemForm';
+import { ImageUploader } from '@/components/pages/addItemPage/ImageUploader';
+
+/**
+ * 新規に商品情報をバックエンドのDBに送信するページ
+ * - 商品情報のバリデーションスキーマを取得するコンポーネント
+ * @returns
+ */
 const AddNewItemPage = () => {
-  // カスタムフックを利用している
-  // itemFormStateにフォームの入力情報、handleFormSubmitでmongoDBに商品データを送信する処理
-  const { handleFormSubmit } = useItemForm();
-
-  // バリデーションスキーマを取得するコンポーネント
-  const schema = AddNewItemValidatedSchema();
-
-  // textfieldにバリデーションを渡すため
-  const formMethods = useForm({
-    defaultValues: {
-      loading: false,
-      itemCategoryName: 'テント',
-      itemTags: [],
-      colorTags: [],
-      brandName: 'ogawa',
-    },
-    resolver: yupResolver(schema),
-  });
-
-  const detailFormMethods = useDetailFormMethods(formMethods);
-
-  // バリデーションチェックを通ったdataをhandleFormSubmit関数でmongoDBに商品データを送信する
-  const onSubmit = async (data: any) => {
-    console.log('onSubmit function is called');
-
-    // 両方のフォームメソッドのデータを取得
-    const baseFormData = formMethods.getValues();
-    const detailFormData = detailFormMethods.getValues();
-
-    try {
-      await handleFormSubmit(baseFormData, detailFormData);
-      console.log('Data submitted successfully');
-    } catch (error) {
-      console.error('Error submitting data:', error);
-    }
-  };
-
-  const wrappedOnSubmit = (data: any) => {
-    console.log('1');
-    detailFormMethods.handleSubmit(onSubmit)(data);
-  };
+  const { formMethods, submitBothForms, detailFormMethods, loading } =
+    useAddNewItemForm();
 
   return (
-    <AddNewItemPageTemplate>
-      <form onSubmit={formMethods.handleSubmit(wrappedOnSubmit)}>
-        <BaseItemDataForm formMethods={formMethods} />
-        <CategoryDetailList
-          formMethods={formMethods}
-          detailFormMethods={detailFormMethods}
-        />
-        {/* 送信ボタン */}
-        <SubmitButton
-          loading={formMethods.watch('loading') || false}
-          text={'データ送信'}
-        />
-      </form>
-    </AddNewItemPageTemplate>
+    <FormProvider formMethods={formMethods}>
+      <AddNewItemPageTemplate>
+        <form onSubmit={submitBothForms}>
+          {/* 画像アップロード */}
+          <ImageUploader />
+          {/* 全商品共有の基本情報 */}
+          <BaseItemDataForm />
+          {/* カテゴリー毎の詳細情報 */}
+          <CategoryDetailList detailFormMethods={detailFormMethods} />
+          {/* 送信ボタン */}
+          <SubmitButton type={'button'} loading={loading} text={'データ送信'} />
+        </form>
+      </AddNewItemPageTemplate>
+    </FormProvider>
   );
 };
 

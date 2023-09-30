a11c3aee17ccd3ffad70c55ba779f19bb6a7b33c
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 22:48:53 +0900

    useUserLoginとuseUserRegisterで同じ処理を行っていたので、共通部をuseUserAuthとして実装しました。

---
+++ frontend/hooks/UserAuth/useUserAuth.ts
@@ -0,0 +1,98 @@
+// useUserAuth.ts (新規ファイル)
+
+import { useState } from 'react';
+import { Path, useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useSetRecoilState } from 'recoil';
+import { useApiRequest } from '@/hooks/api/useApiRequest';
+import { userState } from '@/components/shares/atoms/state/userState';
+
+/**
+ * フォームデータをAPIリクエストデータに変換する関数の型
+ * 
+ * @template T フォームデータの型
+ * @param  data 変換前のフォームデータ
+ * @returns 変換後のAPIリクエストデータ
+ */
+type DataTransformer<T> = (data: T) => Record<string, any>;
+
+/**
+ * 共通のユーザー認証処理を行うカスタムフック
+ *
+ * @remarks
+ * このフックは、ログインやユーザー登録などのユーザー認証処理で使用されます。
+ * - フォームのバリデーション
+ * - APIの呼び出し
+ * - ローディング状態の管理
+ * - ユーザー情報の更新
+ * などの処理を行います。
+ *
+ * @template T フォームデータの型
+ * @param schema Yupによるバリデーションスキーマ
+ * @param apiEndpoint APIエンドポイントのURL
+ * @param transformData フォームデータをAPIに適した形に変換する関数
+ * @returns フォームメソッド、onSubmit関数、ローディング状態
+ */
+export const useUserAuth = <T extends Record<string, any>>(
+    schema: any,
+    apiEndpoint: string,
+    transformData: DataTransformer<T>
+) => {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+    const { sendRequest } = useApiRequest();
+    const setUser = useSetRecoilState(userState);
+
+    const formMethods = useForm<T>({
+        resolver: yupResolver(schema),
+    });
+
+    // スネークケースをキャメルケースに変換
+    const convertSnakeToCamelCase = (str: string): string => {
+        return str.replace(/([-_][a-z])/g, (group) =>
+            group.toUpperCase().replace('-', '').replace('_', ''),
+        );
+    };
+
+    /**
+    * フォームの送信処理
+    * TODO Tについての理解が出来ていない。勉強が必要
+    * 
+    * - 認証に成功した場合は、アクセストークンとリフレッシュトークンをローカルストレージに保存
+    * - transformData関数を用いて、フォームデータをAPIリクエストに適した形に変換
+    * 
+    * @param data フォームデータ（型 T）
+    */
+    const onSubmit = async (data: T) => {
+        try {
+            setLoading(true);
+            const transformedData = transformData(data);
+            const response = await sendRequest('post', apiEndpoint, transformedData);
+            if (response && (response.status === 200 || response.status === 201)) {
+                localStorage.setItem('access_token', response.data.access_token);
+                localStorage.setItem('refresh_token', response.data.refresh_token);
+                setUser(response.data.user);
+                setLoading(false);
+                router.push('/');
+            }
+        } catch (err: any) {
+            setLoading(false);
+
+            // エラーコードが422の場合、バリデーションエラーとして扱う
+            if (err.response && err.response.status === 422) {
+                const validationErrors = err.response.data as Record<string, string[]>;
+
+                // サーバーからのエラーメッセージをreact-hook-formに登録
+                for (const [field, messages] of Object.entries(validationErrors)) {
+                    const camelField = convertSnakeToCamelCase(field);
+                    formMethods.setError(camelField as unknown as Path<T>, {
+                        type: 'manual',
+                        message: messages[0],
+                    });
+                }
+            }
+        }
+    };
+    return { formMethods, onSubmit, loading };
+};

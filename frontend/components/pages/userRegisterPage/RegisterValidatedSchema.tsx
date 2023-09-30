64697f55ea7c2cabb37b8cc33178f7d7293a6a52
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 22:50:01 +0900

    関数となっていたのを修正しました。

--- frontend/components/pages/userRegisterPage/RegisterValidatedSchema.tsx
+++ frontend/components/pages/userRegisterPage/RegisterValidatedSchema.tsx
@@ -1,31 +1,27 @@
 import * as yup from 'yup';
 
-export const RegisterValidatedSchema = () => {
-  const schema = yup.object().shape({
-    userName: yup
-      .string()
-      .max(30, 'ユーザー名は30文字以内です')
-      .required('ユーザー名は必須です'),
-    email: yup
-      .string()
-      .email('有効なメールアドレスを入力してください。')
-      .required('メールアドレスは必須です。'),
-    password: yup
-      .string()
-      .min(6, 'パスワードは最低6文字必要です')
-      .max(30, 'パスワードは30文字以内です')
-      .required('パスワードは必須です。')
-      .matches(
-        /^(?=.*[a-zA-Z])(?=.*[0-9])/,
-        'パスワードは少なくとも1つの英字と1つの数字を含む必要があります',
-      ),
-    passwordConfirmation: yup
-      .string()
-      .test('passwords-match', 'パスワードが一致しません', function (value) {
-        return this.parent.password === value;
-      })
-      .required('確認用パスワードは必須です'),
-    loading: yup.boolean(),
-  });
-  return schema;
-};
+export const RegisterValidatedSchema = yup.object().shape({
+  userName: yup
+    .string()
+    .max(30, 'ユーザー名は30文字以内です')
+    .required('ユーザー名は必須です'),
+  email: yup
+    .string()
+    .email('有効なメールアドレスを入力してください。')
+    .required('メールアドレスは必須です。'),
+  password: yup
+    .string()
+    .min(6, 'パスワードは最低6文字必要です')
+    .max(30, 'パスワードは30文字以内です')
+    .required('パスワードは必須です。')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])/,
+      'パスワードは少なくとも1つの英字と1つの数字を含む必要があります',
+    ),
+  passwordConfirmation: yup
+    .string()
+    .test('passwords-match', 'パスワードが一致しません', function (value) {
+      return this.parent.password === value;
+    })
+    .required('確認用パスワードは必須です'),
+});

4ba6c55c6081f4ea2dce9a3e3e8be1be26e73669
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 23:01:23 +0900

    lang=jaを追記しました。

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="ja">
         <Head>
           {/* <!-- // Zen Old Mincho　フォントのよみこみ --> */}
           <link

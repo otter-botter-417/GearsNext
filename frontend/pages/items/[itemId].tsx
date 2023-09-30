347d34e7eefdd3fca61dbf83f7eb5bc3492a6fa1
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-01 03:27:14 +0900

    開発用の不要なコードを削除しました。

--- frontend/pages/items/[itemId].tsx
+++ frontend/pages/items/[itemId].tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import fetch from 'node-fetch';
 import Box from '@mui/material/Box';
-import { Grid } from '@mui/material';
+import { Grid, useMediaQuery } from '@mui/material';
 import { useRouter } from 'next/router';
 import { GetStaticPropsContext } from 'next';
 
 import { useGetItemDataApi } from '@/hooks/api/useItemShowApi';
-import { useFlashBackgroundOnRender } from '@/hooks/useFlashBackgroundOnRender';
 
 import { ItemBaseData } from '@/components/shares/organisms/ItemBaseData';
 import { LayoutImageList } from '@/components/shares/organisms/LayoutImageList';
@@ -58,48 +57,45 @@ export async function getStaticPaths() {
 export const ItemPage = ({ itemDetail }: { itemDetail: ItemDataType }) => {
   const router = useRouter();
   const itemId = router.query.itemId;
-  const backgroundColor = useFlashBackgroundOnRender();
 
   useGetItemDataApi(itemId as string);
 
   return (
-    <div className="flashBackground" style={{ backgroundColor }}>
-      <Box sx={{ width: '80%', margin: '0 auto' }}>
-        <Grid container>
-          <Grid item xs={12} sm={7}>
-            {/* 商品画像と商品名とブランド名*/}
-            <ItemDetailHeader
-              itemName={itemDetail.itemName}
-              brandName={itemDetail.brandName}
-              imageName={itemDetail.imageName}
-            />
-          </Grid>
-          <Grid xs={12} sm={5}>
-            {/* いいね　と　共有ボタンの表示コンポーネント　共有時の情報を渡す */}
-            <ItemDetailPageButtons
-              itemId={itemId}
-              itemName={itemDetail.itemName}
-            />
-            {/* 商品の基本情報 */}
-            <ItemBaseData
-              price={itemDetail.price}
-              openSizes={itemDetail.openSize}
-              storageSizes={itemDetail.storageSize}
-              weight={itemDetail.weight}
-            />
-            {/* カテゴリー毎の詳細情報 */}
-            <CategoryDetailSwitcher
-              categoryName={itemDetail.categoryName}
-              itemAttributes={itemDetail.itemAttributes}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            {/* レイアウトの画像リスト */}
-            {/* <LayoutImageList layouts={itemDetail.layouts} height={'700px'} /> */}
-          </Grid>
+    <Box sx={{ width: '80%', margin: '0 auto' }}>
+      <Grid container>
+        <Grid item xs={12} md={7}>
+          {/* 商品画像と商品名とブランド名*/}
+          <ItemDetailHeader
+            itemName={itemDetail.itemName}
+            brandName={itemDetail.brandName}
+            imageName={itemDetail.imageName}
+          />
         </Grid>
-      </Box>
-    </div>
+        <Grid item sm={12} md={5}>
+          {/* いいね　と　共有ボタンの表示コンポーネント　共有時の情報を渡す */}
+          <ItemDetailPageButtons
+            itemId={itemId}
+            itemName={itemDetail.itemName}
+          />
+          {/* 商品の基本情報 */}
+          <ItemBaseData
+            price={itemDetail.price}
+            openSizes={itemDetail.openSize}
+            storageSizes={itemDetail.storageSize}
+            weight={itemDetail.weight}
+          />
+          {/* カテゴリー毎の詳細情報 */}
+          <CategoryDetailSwitcher
+            categoryName={itemDetail.categoryName}
+            itemAttributes={itemDetail.itemAttributes}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          {/* レイアウトの画像リスト */}
+          <LayoutImageList layouts={itemDetail.layouts} height={'700px'} />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 export default ItemPage;

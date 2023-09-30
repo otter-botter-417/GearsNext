b729e671b73302e69c22e025606b2843fd728e96
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 20:50:49 +0900

    AuthFormコンポーネントを作成し、propsを渡し、ラップするように変更しました。

--- frontend/components/pages/userRegisterPage/RegisterForm.tsx
+++ frontend/components/pages/userRegisterPage/RegisterForm.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
-import { ValidationInputFields } from '@/components/shares/atoms/form/ValidationInputFields';
+import { useUserRegister } from '@/hooks/UserAuth/useUserRegister';
+import { FormProvider } from '@/hooks/useFormMethods ';
+
+import { AuthForm } from '@/components/pages/userRegisterPage/AuthForm';
 import { RegisterInputFormFieldsList } from '@/components/pages/userRegisterPage/RegisterInputFormFieldsList';
 
 /**
- * ユーザー登録用のフォームコンポーネント
+ * 新規登録フォーム
  *
- * @example
- * <RegisterForm/>
+ * @remarks
+ * このコンポーネントは新規登録ページで使用され、以下の要素を含みます：
+ * - ユーザー名
+ * - メールアドレス
+ * - パスワード
+ * - パスワードの確認
+ *
+ * AuthFormコンポーネントを内部で使用しており、認証に必要なUIとロジックが含まれています。
+ *
+ * @returns ユーザー新規登録フォーム
  */
-const RegisterForm = () => {
+export const RegisterForm = () => {
+  const { formMethods, onSubmit } = useUserRegister();
   return (
-    <ValidationInputFields
-      inputFormFieldsList={RegisterInputFormFieldsList()}
-    />
+    <FormProvider formMethods={formMethods}>
+      <AuthForm
+        inputFormFieldsList={RegisterInputFormFieldsList}
+        onSubmit={onSubmit}
+        buttonText="新規登録"
+      />
+    </FormProvider>
   );
 };
-
-export default RegisterForm;

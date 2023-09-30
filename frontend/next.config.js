62110920484c976adf6b4fe7dbd202e35b9466ba
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 23:02:33 +0900

    domainsにS3のURLを追加しました。

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: false,
   images: {
     domains: [
+      's3-ap-northeast-1.amazonaws.com',
       'gears-images.s3.ap-northeast-1.amazonaws.com',
       'gears-item-images.s3.ap-northeast-1.amazonaws.com',
     ],

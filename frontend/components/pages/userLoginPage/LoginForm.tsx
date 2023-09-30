9c1137d3197ee33c008556b641476d501a67c919
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 21:09:59 +0900

    AuthFormコンポーネントを作成し、propsを渡し、ラップするように変更しました。

--- frontend/components/pages/userLoginPage/LoginForm.tsx
+++ frontend/components/pages/userLoginPage/LoginForm.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 
-import { ValidationInputFields } from '@/components/shares/atoms/form/ValidationInputFields';
-import { UseFormReturn } from 'react-hook-form';
-import { LoginInputFormFieldsList } from './LoginInputFormFieldsList';
+import { FormProvider } from '@/hooks/useFormMethods ';
+import { AuthForm } from '@/components/shares/organisms/AuthForm';
+import { LoginInputFormFieldsList } from '@/components/pages/userLoginPage/LoginInputFormFieldsList';
+import { useUserLogin } from '@/hooks/UserAuth/useUserLogin';
 
-interface ItemInformationFieldsProps {
-  formMethods: UseFormReturn<any>;
-}
-
-const LoginForm: React.FC<ItemInformationFieldsProps> = ({ formMethods }) => {
+/**
+ * ログインフォーム
+ *
+ * @remarks
+ * このコンポーネントはログインページで使用され、以下の要素を含みます：
+ * - メールアドレス
+ * - パスワード
+ *
+ * AuthFormコンポーネントを内部で使用しており、認証に必要なUIとロジックが含まれています。
+ *
+ * @returns ユーザーログインフォーム
+ */
+export const LoginForm = () => {
+  const { formMethods, onSubmit } = useUserLogin();
   return (
-    <>
-      <ValidationInputFields inputFormFieldsList={LoginInputFormFieldsList()} />
-    </>
+    <FormProvider formMethods={formMethods}>
+      <AuthForm
+        inputFormFieldsList={LoginInputFormFieldsList}
+        onSubmit={onSubmit}
+        buttonText="ログイン"
+      />
+    </FormProvider>
   );
 };
-
-export default LoginForm;

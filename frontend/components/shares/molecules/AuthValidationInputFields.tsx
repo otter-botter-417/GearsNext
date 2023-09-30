8b909f9d40f5d63d2f7254fd91e7560beb8b3d5a
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 20:56:11 +0900

    Login,Registerページで共有される、バリデーション付きテキストフィールドを実装しました。

---
+++ frontend/components/shares/molecules/AuthValidationInputFields.tsx
@@ -0,0 +1,49 @@
+import { TextFieldWithValidation } from '@/components/shares/atoms/form/TextFieldWithValidation';
+import { useFormMethods } from '@/hooks/useFormMethods ';
+import { Box } from '@mui/system';
+import { FC } from 'react';
+
+interface AuthValidationInputFieldsProps {
+  inputFormFieldsList: {
+    name: string;
+    label: string;
+  }[];
+  formMethods: ReturnType<typeof useFormMethods>;
+}
+
+/**
+ * ユーザー認証用のバリデーション付きテキストフィールドコンポーネント
+ *
+ * @remarks
+ * このコンポーネントは、複数の入力フィールドにバリデーションロジックを適用します。
+ * 各フィールドは`TextFieldWithValidation`を使用しています。
+ *
+ * @param inputFormFieldsList - フォームフィールドの名前とラベルの配列
+ *
+ * @example
+ * ```tsx
+ * <AuthValidationInputFields
+ *   inputFormFieldsList={inputFields}
+ * />
+ * ```
+ *
+ * @returns バリデーション付きテキストフィールドのリスト
+ */
+export const AuthValidationInputFields: FC<AuthValidationInputFieldsProps> = ({
+  inputFormFieldsList,
+  formMethods,
+}) => {
+  return (
+    <>
+      {inputFormFieldsList.map((field) => (
+        <Box key={field.name} paddingBottom={2} width={'300px'}>
+          <TextFieldWithValidation
+            name={field.name}
+            label={field.label}
+            formMethods={formMethods}
+          />
+        </Box>
+      ))}
+    </>
+  );
+};

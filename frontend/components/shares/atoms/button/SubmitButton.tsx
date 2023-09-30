cd72e9d75900686479dd12dcfcded29a9d351434
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 22:58:14 +0900

    typeをpropsから受け取るように修正しました。

---
+++ frontend/components/shares/atoms/button/SubmitButton.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import { LoadingButton } from '@mui/lab';
+
+interface SubmitButtonProps {
+  loading: boolean;
+  text: string;
+  type?: 'submit' | 'button'; // Add this line
+}
+
+/**
+ * LoadingButtonをラッピングして、ローディング状態やボタンテキストなどを
+ * カスタマイズできるようにしたコンポーネントです。
+ *
+ * @param loading ローディング状態
+ * @param text ボタンテキスト
+ * @param type ボタンの種類 (デフォルトは'submit')
+ * @returns ローディング状態やボタンテキストなどをカスタマイズできるボタン
+ * @example
+ * <SubmitButton loading={loading} text="送信" />
+ */
+export const SubmitButton: FC<SubmitButtonProps> = ({
+  loading,
+  text,
+  type = 'submit',
+}) => {
+  return (
+    <LoadingButton
+      type={type}
+      variant="outlined"
+      loading={loading}
+      sx={{ mt: 4 }}
+    >
+      {text}
+    </LoadingButton>
+  );
+};

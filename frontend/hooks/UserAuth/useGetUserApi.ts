047913666967dcc1f410a1cfaab24cbd93adc1b2
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-09-30 22:55:11 +0900

    毎回user情報がリセットされてしまっていたため、access_tokenがある場合に、一時的にローディング中として保持するよう変更。

--- frontend/hooks/UserAuth/useGetUserApi.ts
+++ frontend/hooks/UserAuth/useGetUserApi.ts
@@ -6,23 +6,34 @@ import { useApiRequest } from '@/hooks/api/useApiRequest';
 import { userState } from '@/components/shares/atoms/state/userState';
 
 /**
- * ユーザーが認証済みかどうかをチェックするカスタムフック
- * - ブラウザ環境かどうかを確認。サーバーサイドでは動作しない。
- * - アクセストークンが存在する場合、APIを呼び出してユーザー情報を取得。
- * - ユーザー情報が取得できた場合はRecoilのuserStateを更新。
+ * ユーザー認証状態を取得および更新するカスタムフック
+ * 
+ * - SSR環境では動作しないため、ブラウザ環境でのみ動作。
+ * - ローカルストレージに保存されているアクセストークンを使用して、
+ *   ユーザー情報をAPIから取得。
+ * - APIからのレスポンスに基づいて、RecoilのuserStateを更新。
  */
 export const useGetUserApi = () => {
   const { sendRequest } = useApiRequest();
   const setUser = useSetRecoilState(userState);
 
+
   useEffect(() => {
     if (typeof window === 'undefined') return;
     const token = localStorage.getItem('access_token');
+
+    // アクセストークンが存在する場合、一時的にユーザー情報をロード中として設定
     if (token) {
+      setUser({ userId: -1, userName: 'LOADING' });
       getUserInfo();
     }
   }, []);
 
+  /**
+   * APIを呼び出してユーザー情報を取得
+   * - 成功時：RecoilのuserStateを更新
+   * - 失敗時：ローカルストレージからトークンを削除し、userStateをnullに設定
+   */
   const getUserInfo = async () => {
     try {
       const response = await sendRequest('GET', 'user/me',);
@@ -30,6 +41,8 @@ export const useGetUserApi = () => {
         const data = response.data;
         setUser(data);
       } else if (response) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
         setUser(null);
       }
     } catch (err: any) {

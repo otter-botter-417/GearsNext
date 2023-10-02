ac20f30d31263c15fed15bc8f8632618cb9011fe
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-03 06:16:01 +0900

    全てのサブカテゴリーが選択できるように変更しました。 close #113

--- frontend/components/pages/ItemSearchPage/SubCategorySelector.tsx
+++ frontend/components/pages/ItemSearchPage/SubCategorySelector.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { SUB_CATEGORY_OPTIONS } from '@/components/types/SubCategoryOptions';
 
 import { categoryValueState } from '@/components/shares/atoms/state/categoryValueState';
 import { PullDownSelector } from '@/components/shares/molecules/PullDownSelector';
 import { subCategoryValueState } from '@/components/shares/atoms/state/subCategoryValueState';
+import { useItemListFilters } from '@/hooks/ItemSearchPage/useItemListFilters';
 
 /**
  * カテゴリーの値によって、サブカテゴリーの選択肢を変更します。
@@ -14,7 +15,10 @@ import { subCategoryValueState } from '@/components/shares/atoms/state/subCatego
  */
 export const SubCategorySelector = () => {
   const categoryValue = useRecoilValue(categoryValueState);
-  const setSubCategoryValue = useSetRecoilState(subCategoryValueState);
+  const [subCategoryValue, setSubCategoryValue] = useRecoilState(
+    subCategoryValueState,
+  );
+  const { applyItemFilters } = useItemListFilters();
 
   /**
    * カテゴリーが変更されたとき、サブカテゴリーを初期化します。
@@ -27,9 +31,15 @@ export const SubCategorySelector = () => {
     setSubCategoryValue('');
     return null;
   }
-  const category = categoryValue as keyof typeof SUB_CATEGORY_OPTIONS;
-  const subCategoryOptions = SUB_CATEGORY_OPTIONS[category] || [];
-
+  let subCategoryOptions = [];
+  if (categoryValue === 'すべてのカテゴリー') {
+    subCategoryOptions = ['すべてのサブカテゴリー'];
+  } else {
+    const category = categoryValue as keyof typeof SUB_CATEGORY_OPTIONS;
+    subCategoryOptions = ['すべてのサブカテゴリー'].concat(
+      SUB_CATEGORY_OPTIONS[category] || [],
+    );
+  }
   return (
     <PullDownSelector
       options={subCategoryOptions}

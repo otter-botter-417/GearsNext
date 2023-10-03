516024513f5df07f5718bd451b14d2a3768dd327
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-04 05:32:30 +0900

    API_BASE_URLを変更しました！

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -5,8 +5,8 @@ export const STATUS_UNAUTHORIZED = 401;
 export const LOGIN_ALERT_TIMEOUT = 2000;
 export const ICON_SIZE = 30;
 export const BASE_URL = 'http://localhost:3000';
-// export const API_BASE_URL = 'https://www.gears-outdoor.net/api/' //aws
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/' //local
+export const API_BASE_URL = 'https://www.gears-outdoor.net/api/' //aws
+// export const API_BASE_URL = 'http://127.0.0.1:8000/api/' //local
 export const ITEM_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/item/';
 export const LAYOUT_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/layout/image/';
 

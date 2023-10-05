7e4737c46f6201fef9d9165824b51af266b28ea5
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-06 03:18:15 +0900

    投稿完了後に最新のコメントを受け取って更新するように変更しました。

--- frontend/hooks/api/useCreateComment.ts
+++ frontend/hooks/api/useCreateComment.ts
@@ -1,6 +1,8 @@
 
+import { layoutDetailState } from '@/components/shares/atoms/state/layoutDetailState';
 import { useApiRequest } from '@/hooks/api/useApiRequest';
 import { useErrorHandler } from '@/hooks/api/useErrorHandler';
+import { useSetRecoilState } from 'recoil';
 
 /**
  * レイアウトの新規コメント投稿を管理するカスタムフック。
@@ -8,9 +10,11 @@ import { useErrorHandler } from '@/hooks/api/useErrorHandler';
 export const useCreateComment = () => {
     const { sendRequest } = useApiRequest();
     const { handleError, clearError } = useErrorHandler();
+    const setLayoutDetail = useSetRecoilState(layoutDetailState);
 
     /**
      * 新規コメントを投稿する。
+     * - 成功した場合は、更新後のコメント一覧を受け取るので、レイアウト詳細のコメント一覧を更新する。
      * @param commentText - コメントのテキスト
      * @param commentParentId - 親コメントのID（なければnull）
      * @param layoutId - レイアウトのID
@@ -30,6 +34,19 @@ export const useCreateComment = () => {
                 handleError(null, 'レスポンスが無効です。');
                 return false;
             }
+
+            // レイアウト詳細のコメント一覧を更新する
+            const updatedComments = response.data.data;
+            setLayoutDetail((prevLayoutDetail) => {
+                if (!prevLayoutDetail) {
+                    return null;
+                }
+                return {
+                    ...prevLayoutDetail,
+                    comments: updatedComments
+                };
+            });
+
             clearError();
             return true;
         } catch (error) {

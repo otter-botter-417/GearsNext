cfa636d967f1f7fd5bebdcc146ca69f4a94cd61f
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-06 03:28:40 +0900

    フィールドを空白にするボタンを設置しました。

--- frontend/components/pages/addLayoutPage/ItemNameSearchField.tsx
+++ frontend/components/pages/addLayoutPage/ItemNameSearchField.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import ClearIcon from '@mui/icons-material/Clear';
 
 import { itemSearchQueryState } from '@/components/shares/atoms/state/itemSearchQueryState';
 
 /**
- * このコンポーネントは、ImageMapTagEditor内で呼び出されます。
  * 商品名で検索するためのテキストフィールドを提供します。
+ * このコンポーネントは、ImageMapTagEditor内で呼び出されます。
  * ユーザーがテキストフィールドに入力すると、Recoil State（itemSearchQueryState）が更新されます。
+ * ユーザーがテキストフィールドのリセットボタンをクリックすると、Recoil State（itemSearchQueryState）が初期化されます。
  */
 export const ItemNameSearchField = () => {
   const [itemSearchQuery, setItemSearchQuery] =
     useRecoilState(itemSearchQueryState);
+
+  const inputTextReset = () => {
+    setItemSearchQuery('');
+  };
 
   return (
     <TextField
@@ -19,8 +27,20 @@ export const ItemNameSearchField = () => {
       label="商品名で検索"
       variant="outlined"
       value={itemSearchQuery}
-      sx={{
-        width: 1,
+      sx={{ width: 1 }}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="clear search query"
+              edge="end"
+              onClick={inputTextReset}
+              size="large"
+            >
+              <ClearIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
       }}
     />
   );

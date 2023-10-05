71a9d43ee11d72091b7979c3b7947749b58828a0
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-05 22:54:04 +0900

    画像を選択されたときの処理に変更があったため、不要な引数とその取得を削除しました。

--- frontend/hooks/useImageSelector.ts
+++ frontend/hooks/useImageSelector.ts
@@ -1,13 +1,8 @@
-import { ChangeEvent, FC, RefObject } from 'react';
+import { ChangeEvent } from 'react';
 import { useSetRecoilState } from 'recoil';
 
 import { imageFileState } from '@/components/shares/atoms/state/imageFileState';
 import { imagePreviewUrlState } from '@/components/shares/atoms/state/imagePreviewUrlState';
-import { imageOriginalSizeState } from '@/components/shares/atoms/state/imageOriginalSizeState';
-
-type UseImageSelectorProps = {
-    imageRef: RefObject<HTMLImageElement>;
-};
 
 /**
  * 画像選択時の処理を管理するカスタムフック。
@@ -19,15 +14,13 @@ type UseImageSelectorProps = {
  * @example
  * const { handleFileChange } = useImageSelector({ imageRef });
  */
-export const useImageSelector = ({ imageRef }: UseImageSelectorProps) => {
+export const useImageSelector = () => {
     const setImageFile = useSetRecoilState(imageFileState);
     const setImagePreviewUrl = useSetRecoilState(imagePreviewUrlState);
-    const setImageOriginalSize = useSetRecoilState(imageOriginalSizeState);
 
     /**
      * 画像ファイルが選択されたときの処理。
      * この関数は、選択された画像ファイルとそのプレビューURLをRecoil Stateに保存します。
-     * さらに、画像の表示サイズもRecoil Stateに保存します。
      *
      * @param {ChangeEvent<HTMLInputElement>} e - input type="file"からのイベントオブジェクト。
      */
@@ -38,22 +31,6 @@ export const useImageSelector = ({ imageRef }: UseImageSelectorProps) => {
         const url = URL.createObjectURL(selectedFile);
         setImageFile(selectedFile);
         setImagePreviewUrl(url);
-
-        // 新しいImageオブジェクトを作成し、画像の表示サイズを取得
-        const img = new Image();
-        img.onload = () => {
-            imageRef.current?.addEventListener('load', () => {
-                const displaySize = {
-                    width: imageRef.current?.clientWidth || 0,
-                    height: imageRef.current?.clientHeight || 0,
-                };
-                setImageOriginalSize(displaySize);
-            });
-            imageRef.current?.setAttribute('src', url);
-        };
-        img.src = url;
-
-
     }
     return { handleFileChange };
 };

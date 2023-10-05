510abd9afffbf968fe1d8f573db08c901c1c9aae
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-06 03:40:27 +0900

    コメントを返す際のリソースコントローラーを実装しました。

---
+++ backend/app/Http/Resources/CommentResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * コメントを提供するリソースクラスです。
+ * このクラスではコメントの詳細をJSON形式で返却します。
+ * 
+ * CommentControllerのshow,storeメソッドで使用します。
+ */
+class CommentResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'commentId' => $this->comment_id,
+            'layoutId' => $this->layout_id,
+            'content' => $this->content,
+            'userName' => $this->user->user_name,
+            'parentId' => $this->parent_id,
+            'createdAt' => $this->created_at,
+        ];
+    }
+}

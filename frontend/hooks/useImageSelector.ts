7e8ec1c824918ca7b533bd5730a1696820fc7612
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-06 03:15:01 +0900

    画像のオリジナルのサイズを取得する処理を変更しました。

--- frontend/hooks/useImageSelector.ts
+++ frontend/hooks/useImageSelector.ts
@@ -1,12 +1,14 @@
 import { ChangeEvent } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
 import { imageFileState } from '@/components/shares/atoms/state/imageFileState';
 import { imagePreviewUrlState } from '@/components/shares/atoms/state/imagePreviewUrlState';
+import { imageOriginalSizeState } from '@/components/shares/atoms/state/imageOriginalSizeState';
 
 /**
  * 画像選択時の処理を管理するカスタムフック。
  * このフックは、画像ファイルの選択、プレビュー表示、およびその情報のRecoil Stateへの保存を行います。
+ * 画像のオリジナルサイズを取得するために、画像のDOM要素への参照を受け取ります。
  * 
  * @param {RefObject<HTMLImageElement>} imageRef - 画像のDOM要素への参照。
  * @returns {Object} handleFileChange - 画像ファイルが選択されたときに呼び出す関数。
@@ -15,9 +17,9 @@ import { imagePreviewUrlState } from '@/components/shares/atoms/state/imagePrevi
  * const { handleFileChange } = useImageSelector({ imageRef });
  */
 export const useImageSelector = () => {
+    const setImageOriginalSize = useSetRecoilState(imageOriginalSizeState);
     const setImageFile = useSetRecoilState(imageFileState);
     const setImagePreviewUrl = useSetRecoilState(imagePreviewUrlState);
-
     /**
      * 画像ファイルが選択されたときの処理。
      * この関数は、選択された画像ファイルとそのプレビューURLをRecoil Stateに保存します。
@@ -31,6 +33,16 @@ export const useImageSelector = () => {
         const url = URL.createObjectURL(selectedFile);
         setImageFile(selectedFile);
         setImagePreviewUrl(url);
+
+        // 画像のオリジナルサイズを取得する
+        const image = new Image();
+        image.onload = () => {
+            const imageSize = { width: image.width, height: image.height };
+            if (image.height !== 0) {
+                setImageOriginalSize(imageSize);
+            }
+        };
+        image.src = url;
     }
     return { handleFileChange };
 };

b32132996fe6509d0829142a1350d07c00cebb98
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-07 23:16:51 +0900

    レイアウトスタイルの変更をしました。

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import { NextPage } from 'next';
 import { Box } from '@mui/system';
 import { HomeImageGrid } from '@/components/pages/homePage/HomeImageGrid';
 import { useFetchHomeDataApi } from '@/hooks/api/useFetchHomeDataApi';
+import { DEFAULT_PAGE_WIDTH } from '@/components/constants';
 
 const Home: NextPage = () => {
   //データの取得
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
 
   return (
     <Box display="flex" justifyContent="center">
-      <Box width="80%">
+      <Box width="80%" maxWidth={DEFAULT_PAGE_WIDTH}>
         <HomeImageGrid />
       </Box>
     </Box>

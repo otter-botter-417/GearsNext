e3fa5bddd33da19f27b523019e9b5407c50331b7
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-07 23:17:01 +0900

    レイアウトスタイルの変更をしました。

--- frontend/pages/Header.tsx
+++ frontend/pages/Header.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { useRecoilState } from 'recoil';
 import { useRouter } from 'next/router';
 import NextLink from 'next/link';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Box,
-  Grid,
-  useMediaQuery,
-  useTheme,
-} from '@mui/material';
-import {
-  Search as SearchIcon,
-  AddAPhoto as AddAPhotoIcon,
-} from '@mui/icons-material';
+
+// @mui/material components
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+// @mui/material hooks
+import useMediaQuery from '@mui/material/useMediaQuery';
+import useTheme from '@mui/material/styles/useTheme';
+
+// @mui/icons-material
+import SearchIcon from '@mui/icons-material/Search';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 
 import { userState } from '@/components/shares/atoms/state/userState';
 import { LinkButtonWithIcon } from '@/components/shares/molecules/LinkButtonWithIcon';
 import HeaderAuthButton from '@/components/shares/molecules/HeaderAuthButton';
 
-const FONT_COLOR = '#DEDBDB';
+
+const FONT_COLOR = '#dedee0';
 
 const Header = () => {
   const [user, setUser] = useRecoilState(userState);
@@ -39,76 +42,97 @@ const Header = () => {
   };
 
   return (
-    <AppBar
-      position="fixed"
-      style={isSmallScreen ? {} : { paddingLeft: '10%', paddingRight: '10%' }}
-    >
-      <Toolbar variant="dense">
-        <Grid container alignItems="center">
-          <Grid item xs={12} sm={12} md={2}>
-            <Box
-              display={'flex'}
-              justifyContent="space-between"
-              alignItems={'center'}
-            >
-              <NextLink href={`/`}>
-                <Button>
-                  <Typography
-                    variant={isSmallScreen ? 'h6' : 'h4'}
+    <AppBar position="fixed" style={{ width: '100%' }}>
+      <div
+        style={{
+          maxWidth: '1080px',
+          width: '80%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      >
+        <Toolbar variant="dense">
+          <Grid container alignItems="center">
+            <Grid item xs={12} sm={12} md={2}>
+              <Box
+                display={'flex'}
+                justifyContent="space-around"
+                alignItems={'center'}
+              >
+                <NextLink href={`/`}>
+                  <Button>
+                    <Typography
+                      variant={isSmallScreen ? 'h6' : 'h4'}
+                      color={FONT_COLOR}
+                    >
+                      Gears
+                    </Typography>
+                  </Button>
+                </NextLink>
+                {isSmallScreen && (
+                  <HeaderAuthButton
+                    user={user}
+                    handleLogout={handleLogout}
                     color={FONT_COLOR}
-                  >
-                    Gears
-                  </Typography>
-                </Button>
-              </NextLink>
-              {isMediumScreen && (
-                <HeaderAuthButton
-                  user={user}
-                  handleLogout={handleLogout}
-                  color={FONT_COLOR}
-                />
-              )}
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <Grid container>
-              <Grid item xs={12} sm={6}>
-                <Box display="flex" justifyContent="center">
-                  <LinkButtonWithIcon
-                    href="/ItemSearchPage"
-                    endIcon={<SearchIcon color="secondary" fontSize="small" />}
-                    color={FONT_COLOR}
-                  >
-                    商品を探す
-                  </LinkButtonWithIcon>
-                </Box>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Box display="flex" justifyContent="center">
-                  <LinkButtonWithIcon
-                    href="/AddNewLayoutPage"
-                    endIcon={
-                      <AddAPhotoIcon color="secondary" fontSize="small" />
-                    }
-                    color={FONT_COLOR}
-                  >
-                    レイアウトを投稿
-                  </LinkButtonWithIcon>
-                </Box>
+                  />
+                )}
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Grid container justifyContent="space-around">
+                <Grid item xs={12} sm={4} md={6}>
+                  <Box display="flex" justifyContent="center">
+                    <LinkButtonWithIcon
+                      href="/ItemSearchPage"
+                      endIcon={
+                        <SearchIcon color="secondary" fontSize="small" />
+                      }
+                      color={FONT_COLOR}
+                    >
+                      商品を探す
+                    </LinkButtonWithIcon>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sm={4} md={6}>
+                  <Box display="flex" justifyContent="center">
+                    <LinkButtonWithIcon
+                      href="/AddNewLayoutPage"
+                      endIcon={
+                        <AddAPhotoIcon color="secondary" fontSize="small" />
+                      }
+                      color={FONT_COLOR}
+                    >
+                      レイアウトを投稿
+                    </LinkButtonWithIcon>
+                  </Box>
+                </Grid>
+                {!isSmallScreen && isMediumScreen && (
+                  <Grid item sm={4}>
+                    <Box display="flex" justifyContent="center">
+                      <HeaderAuthButton
+                        user={user}
+                        handleLogout={handleLogout}
+                        color={FONT_COLOR}
+                      />
+                    </Box>
+                  </Grid>
+                )}
               </Grid>
             </Grid>
+            {!isMediumScreen && (
+              <Grid item xs={2}>
+                <Box display="flex" justifyContent="flex-end">
+                  <HeaderAuthButton
+                    user={user}
+                    handleLogout={handleLogout}
+                    color={FONT_COLOR}
+                  />
+                </Box>
+              </Grid>
+            )}
           </Grid>
-          {!isMediumScreen && (
-            <Grid item xs={2}>
-              <HeaderAuthButton
-                user={user}
-                handleLogout={handleLogout}
-                color={FONT_COLOR}
-              />
-            </Grid>
-          )}
-        </Grid>
-      </Toolbar>
+        </Toolbar>
+      </div>
     </AppBar>
   );
 };

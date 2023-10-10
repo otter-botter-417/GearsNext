a9755d299456803d2f5896ec83c2a0a66f8ac195
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-10-10 22:44:19 +0900

    引数を追加し、リダイレクトを行うか選択出来るように変更しました。

--- frontend/hooks/UserAuth/useAuthGuard.ts
+++ frontend/hooks/UserAuth/useAuthGuard.ts
@@ -4,14 +4,14 @@ import { useRouter } from "next/router";
  * ユーザー認証を行うカスタムフック
  * トークンが存在しない場合、ログインページにリダイレクトする
  */
-export const useAuthGuard = () => {
+export const useAuthGuard = (isRedirect: boolean, redirectPath: string = '/') => {
     const router = useRouter();
 
     if (typeof window === 'undefined') return false;
 
     const token = localStorage.getItem('access_token');
     if (!token) {
-        router.push('/UserLoginPage');
+        isRedirect && router.push(redirectPath);
         return false;
     }
     return true;

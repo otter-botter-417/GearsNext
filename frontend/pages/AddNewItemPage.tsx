d80d225a8cfe58a91ea98e621bf50603059ca996
Ubuntu <ubuntu@ip-172-31-36-42.ap-northeast-1.compute.internal>
2023-10-15 19:42:40 +0000

    送信成功メッセージを実装しました。

--- frontend/pages/AddNewItemPage.tsx
+++ frontend/pages/AddNewItemPage.tsx
@@ -9,6 +9,7 @@ import { SubmitButton } from '@/components/shares/atoms/button/SubmitButton';
 import { useAddNewItemForm } from '@/hooks/useAddNewItemForm';
 import { ImageUploader } from '@/components/pages/addItemPage/ImageUploader';
 import { ErrorAlert } from '@/components/shares/molecules/ErrorAlert';
+import { AlertSeccess } from '@/components/shares/molecules/AlertSuccess';
 
 /**
  * 新規に商品情報をバックエンドのDBに送信するページ
@@ -30,6 +31,7 @@ const AddNewItemPage = () => {
           {/* カテゴリー毎の詳細情報 */}
           <CategoryDetailList detailFormMethods={detailFormMethods} />
           <ErrorAlert />
+          <AlertSeccess />
           {/* 送信ボタン */}
           <SubmitButton loading={loading} text={'データ送信'} />
         </form>

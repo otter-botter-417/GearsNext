200af0b62968575df9d70871af7c608156c7eb61
Ubuntu <ubuntu@ip-172-31-36-42.ap-northeast-1.compute.internal>
2023-10-18 02:58:18 +0000

    ヘッダー画像の参照URLを追加しました。

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -7,6 +7,7 @@ export const ICON_SIZE = 30;
 export const BASE_URL = 'https://gears-camp.com';
 export const API_BASE_URL = 'https://gears-camp.com/api/' //aws
 // export const API_BASE_URL = 'http://127.0.0.1:8000/api/' //local
+export const ICON_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/public/icon.png';
 export const ITEM_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/item/';
 export const LAYOUT_IMAGE_BASE_URL = 'https://gears-images.s3.ap-northeast-1.amazonaws.com/layout/image/';
 export const DEFAULT_PAGE_WIDTH = '1300px';

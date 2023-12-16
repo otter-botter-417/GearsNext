eb8bc56a360cc628b6ee2513f8580ec1645a2c32
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-12-16 19:59:00 +0900

    importの修正を行いました。

--- frontend/components/pages/userProfilePage/mainContents/ItemSortControl.tsx
+++ frontend/components/pages/userProfilePage/mainContents/ItemSortControl.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { Box, Typography } from '@mui/material';
 
 import { UserProfilePageContentType } from '@/components/types/UserProfilePageContentType';
-import SortToggle from './SortToggle';
+import SortToggle from '@/components/shares/molecules/SortToggle';
 
 type ItemSortControlProps = {
   sortedItems: UserProfilePageContentType[];
@@ -17,7 +17,7 @@ type ItemSortControlProps = {
  * @param sortedItems: ソートされた UserProfilePageContentType オブジェクトの配列
  * @param sortAscending: 現在のソート順 boolean
  * @param handleSwitchChange: ソート順を切り替えるためのハンドラー関数
- * 
+ *
  * @returns アイテムリストの総数と登録日によるソートスイッチを表示するコンポーネント。
  */
 const ItemSortControl: FC<ItemSortControlProps> = ({
@@ -34,7 +34,9 @@ const ItemSortControl: FC<ItemSortControlProps> = ({
         alignItems: 'center',
       }}
     >
-      <Typography variant="body1" mr={5}>{sortedItems.length}件</Typography>
+      <Typography variant="body1" mr={5}>
+        {sortedItems.length}件
+      </Typography>
       <SortToggle
         switchState={sortAscending}
         handleChange={handleSwitchChange}
